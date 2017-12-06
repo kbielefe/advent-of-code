09582b7e20c2d9da5f068538a38188a9046e1f30
Karl Bielefeldt <karl.bielefeldt@adtran.com>
2017-12-06 10:44:51 -0600

    Use much smaller functions.

--- 2017/3.scala
+++ 2017/3.scala
@@ -2,34 +2,21 @@ import Math.abs
 
 val input = 325489
 
-type Square = (Int, Int)
-
 val offset = Map('n' -> (0,1), 'e' -> (1,0), 's' -> (0,-1), 'w' -> (-1,0))
-val initialExtents = Map('n' -> 0, 'e' -> 0, 's' -> 0, 'w' -> 0)
-val initial = ('e', 1, initialExtents, (0, 0), Map((0,0) -> 1))
+val nextDir = Map('n' -> 'w', 'w' -> 's', 's' -> 'e', 'e' -> 'n')
 
-def spiral(f: (Int, Square, Map[Square, Int]) => Int) = Stream.iterate(initial){case (dir, value, extents, (x, y), squares) =>
+def sideLengths = Stream.from(1).flatMap{x => Stream(x,x)}
+
+def turns = sideLengths.flatMap{length => Stream.fill(length-1)(false) ++ Stream(true)}
+
+def dirs = turns.scanLeft('e'){case (dir, turn) => if (turn) nextDir(dir) else dir}
+
+def coords = dirs.scanLeft((0,0)){case ((x, y), dir) =>
   val (xOff, yOff) = offset(dir)
-  val newSquare@(newX, newY) = (x + xOff, y + yOff)
-
-  def newVals(value: Int, f: (Int, Int) => Boolean, newDir: Char) =
-    if (f(value, extents(dir))) (extents + (dir -> value), newDir) else (extents, dir)
-
-  val (newExtents, newDir) = dir match {
-    case 'n' => newVals(newY, _ > _, 'w')
-    case 'e' => newVals(newX, _ > _, 'n')
-    case 's' => newVals(newY, _ < _, 'e')
-    case 'w' => newVals(newX, _ < _, 's')
-  }
-
-  val newValue = f(value, newSquare, squares)
-
-  (newDir, newValue, newExtents, newSquare, squares + (newSquare -> newValue))
+  (x + xOff, y + yOff)
 }
 
-val increment = (value: Int, _: (Int, Int), _: Map[Square, Int]) => value + 1
-
-val sumOfNeighbors = (value: Int, square: (Int, Int), squares: Map[Square, Int]) => {
+def sumOfNeighbors(square: (Int, Int), squares: Map[(Int, Int), Int]): Int = {
   val (x,y) = square
   val neighbors = for {
     neighborX <- x - 1 to x + 1
@@ -38,8 +25,20 @@ val sumOfNeighbors = (value: Int, square: (Int, Int), squares: Map[Square, Int])
   neighbors.sum
 }
 
-val answer1 = spiral(increment).dropWhile(_._2 < input).map(_._4).map{case (x,y) => abs(x)+abs(y)}.head
-println(answer1)
-val answer2 = spiral(sumOfNeighbors).map(_._2).dropWhile(_ <= input).head
-println(answer2)
+def neighbors = coords.scanLeft(Map((0, 0) -> 1)){case (squares, coord) =>
+  squares + (coord -> sumOfNeighbors(coord, squares))
+}.drop(1)
 
+val answer1 = coords
+  .drop(input-1)
+  .map{case (x,y) => abs(x)+abs(y)}
+  .head
+
+println(answer1)
+
+val answer2 = (coords zip neighbors)
+  .map{case (coord, neighbors) => neighbors(coord)}
+  .dropWhile{_ <= input}
+  .head
+
+println(answer2)
