35cc775defb0898daae6dda412819990bb658c50
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2017-12-06 00:34:57 -0600

    Switch to Vector to improve performance.

--- 2017/6.scala
+++ 2017/6.scala
@@ -1,6 +1,6 @@
-val input = "4  10  4 1 8 4 9 14  5 1 14  15  0 15  3 5".split("\\s+").map(_.toInt)
+val input = "4  10  4 1 8 4 9 14  5 1 14  15  0 15  3 5".split("\\s+").map(_.toInt).toVector
 
-def redistribute(input: Array[Int]): Array[Int] = {
+def redistribute(input: Vector[Int]): Vector[Int] = {
   val max = input.max
   val index = input.indexWhere(_ == max)
   val zeroed = input.updated(index, 0)
@@ -11,14 +11,14 @@ def redistribute(input: Array[Int]): Array[Int] = {
 }
  
 val redistributions = Stream.iterate(input)(redistribute)
-val cumulative = redistributions.scanLeft(Set.empty[Array[Int]])(_ + _)
+val cumulative = redistributions.scanLeft(Set.empty[Vector[Int]])(_ + _)
 val zipped = redistributions zip cumulative
-val results = zipped takeWhile {case (x, seen) => !seen.exists(_ sameElements x)}
+val results = zipped takeWhile {case (x, seen) => !seen.contains(x)}
 val answer1 = results.size
 
 println(answer1)
 
 val looped = redistribute(results.last._1)
-val beforeLoop = redistributions takeWhile {!_.sameElements(looped)}
+val beforeLoop = redistributions takeWhile {_ != looped}
 val answer2 = answer1 - beforeLoop.size
 println(answer2)
