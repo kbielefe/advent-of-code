0df66bee8ebfc600bc662c39392c9607559bc391
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2017-12-09 01:20:10 -0600

    Refactor for shorter functions.

--- 2017/9.scala
+++ 2017/9.scala
@@ -1,28 +1,26 @@
 import scala.io.Source
+import Math.max
 
-val input = Source.fromFile("input9.txt")
+val input = Source.fromFile("input9.txt").toStream
 
-val (_, _, score, _, nonCanceled) = input.foldLeft((false, false, 0, 0, 0)){case ((cancel, garbage, score, nesting, nonCanceled), char) =>
-  if (cancel) {
-    (false, garbage, score, nesting, nonCanceled)
-  } else if (garbage) {
-    char match {
-      case '>' => (false, false, score, nesting, nonCanceled)
-      case '!' => (true,  true,  score, nesting, nonCanceled)
-      case _   => (false, true,  score, nesting, nonCanceled + 1)
-    }
-  } else {
-    char match {
-      case '{' => (false, false, score + nesting + 1, nesting + 1, nonCanceled)
-      case '}' => (false, false, score,               nesting - 1, nonCanceled)
-      case '<' => (false, true,  score,               nesting,     nonCanceled)
-      case _   => (false, false, score,               nesting,     nonCanceled)
-    }
-  }
+val cancel = input.scanLeft(0){case (cancel, char) =>
+  if (cancel < 2 && char == '!') 2 else max(0, cancel - 1)
+}.drop(1).map(_ > 0)
+
+val nonCanceled = (cancel zip input) filterNot {_._1} map {_._2}
+
+val isGarbage = nonCanceled.scanLeft(false){case (garbage, char) =>
+  if (char == '>') false else char == '<' || garbage
 }
 
-val answer1 = score
+val nonGarbage = (isGarbage zip nonCanceled) filterNot {_._1} map {_._2} filter {_ != '<'}
+
+val braces = nonGarbage filter {_ != ','}
+
+val nesting = braces.scanLeft(0){case (nesting, char) => if (char == '{') nesting + 1 else nesting - 1}.drop(1)
+
+val answer1 = (nesting zip braces).filter{_._2 == '{'}.map(_._1).sum
 println(answer1)
 
-val answer2 = nonCanceled
+val answer2 = (isGarbage zip nonCanceled).filter{case (isGarbage, char) => isGarbage && (char != '>')}.size
 println(answer2)
