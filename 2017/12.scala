74fcd15c32e4ccc01cb2ab693888c4336c733f5a
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2017-12-12 05:30:50 -0600

    Add 2017/12

---
+++ 2017/12.scala
@@ -0,0 +1,17 @@
+import scala.io.Source
+
+val lines = Source.fromFile("input12.txt").getLines
+val input = lines.map(_ split " <-> ").map{x => (x(0).toInt, x(1).split(", ").map(_.trim.toInt).toSet)}.toMap
+
+def group(root: Int): Set[Int] = {
+  val closure = Stream.iterate(Set(root)){_ flatMap {x => input(x) + x}}
+  val sizes = closure map {_.size}
+  val sizesEqual = (sizes zip (sizes drop 1)).map{case (x, y) => x == y}
+  (closure zip sizesEqual).dropWhile(!_._2).map(_._1).head
+}
+
+val answer1 = group(0).size
+println(answer1)
+
+val answer2 = input.keySet.map(group).size
+println(answer2)
