40be0b32195a128a5801e54b6d59c6a21f98acac
Karl Bielefeldt <karl.bielefeldt@adtran.com>
2017-12-13 13:08:51 -0600

    Trim invalid char from end.

--- 2017/1.scala
+++ 2017/1.scala
@@ -1,6 +1,6 @@
 import scala.io.Source
 
-val input = Source.fromFile("input1.txt").map(_.asDigit).toList
+val input = Source.fromFile("input1.txt").map(_.asDigit).toList.filter{_ != -1}
 
 def zipped(offset: Int) = input zip ((input ++ input) drop offset)
 def answer(offset: Int) = zipped(offset).filter{case (x,y) => x == y}.map{_._1}.sum
