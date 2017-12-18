3b6db03082f572b53b30055b23297cb62dd87622
Karl Bielefeldt <karl.bielefeldt@adtran.com>
2017-12-18 09:40:12 -0600

    Start on part 2.

--- 2017/18.scala
+++ 2017/18.scala
@@ -1,5 +1,6 @@
 import scala.io.Source
 import scala.util.Try
+import java.util.concurrent.{LinkedBlockingQueue => Queue}
 
 val input = Source.fromFile("input18.txt").getLines.map(_ split " ").toVector
 
@@ -20,12 +21,30 @@ def execute = Iterator.iterate((Map.empty[String, Long], Vector.empty[(String, L
     case _     => regs
   }
   val newActions = instruction match {
-    case "snd"                              => actions :+ (("snd", reg(arg1)))
+    case "snd"                     => actions :+ (("snd", reg(arg1)))
     case "rcv" if (reg(arg1) != 0) => actions :+ (("rcv", reg("lastSound")))
-    case _                                  => actions
+    case _                         => actions
   }
   (newRegs + ("pc" -> newPc), newActions)
 }
 
 val answer1 = execute.map(_._2).filterNot(_.isEmpty).dropWhile(_.last._1 == "snd").map(_.last._2).next
 println(answer1)
+
+def execute2(id: Long, from: Queue[Long], to: Queue[Long]) = Iterator.iterate(Map("p" -> id)){case regs =>
+  def reg = getArg(regs) _
+  val pc = reg("pc")
+  val Array(instruction, arg1, arg2) = input(pc.toInt).padTo(3, "")
+  val newPc = if (instruction == "jgz" && reg(arg1) > 0) pc + reg(arg2) else pc + 1
+  val newRegs = instruction match {
+    case "snd" => to.put(reg(arg1)); regs
+    case "rcv" => regs + (arg1 -> from.take)
+    case "set" => regs + (arg1 -> reg(arg2))
+    case "add" => regs + (arg1 -> (reg(arg1) + reg(arg2)))
+    case "mul" => regs + (arg1 -> reg(arg1) * reg(arg2))
+    case "mod" => regs + (arg1 -> reg(arg1) % reg(arg2))
+    case _     => regs
+  }
+  newRegs
+}
+
