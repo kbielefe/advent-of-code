0cb778162ac412f7ea15de1bccd5be867f3198b7
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2017-12-20 08:59:29 -0600

    Include index with Particle and add stub for collisions.

--- 2017/20.scala
+++ 2017/20.scala
@@ -2,14 +2,22 @@ import scala.io.Source
 import Math.abs
 
 type Coord = (Int, Int, Int)
-case class Particle(p: Coord, v: Coord, a: Coord)
+case class Particle(i: Int, p: Coord, v: Coord, a: Coord) {
+  //at^2 + bt + c = 0
+  //(-b (+-) sqrt(b^2 - 4ac))/2a
+  //bt + c = 0
+  //t = -c/b
+  def collisions(other: Particle): List[Int] = ???
+}
+
 object Particle {
-  def apply(line: String): Particle = {
+  def apply(line: String, index: Int): Particle = {
     val Array(p, v, a) = line.split(", ").map(_.drop(3).dropRight(1).split(",").map(_.toInt)).map{case Array(x, y, z) => (x, y, z)}
-    new Particle(p, v, a)
+    new Particle(index, p, v, a)
   }
 }
-val input = Source.fromFile("input20.txt").getLines.map(Particle(_))
 
-val answer1 = input.zipWithIndex.minBy{x => val a = x._1.a; abs(a._1) + abs(a._2) + abs(a._3)}._2
+val input = Source.fromFile("input20.txt").getLines.zipWithIndex.map{case (line, index) => Particle(line, index)}
+
+val answer1 = input.minBy{x => val a = x.a; abs(a._1) + abs(a._2) + abs(a._3)}.i
 println(answer1)
