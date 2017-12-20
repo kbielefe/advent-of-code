5f52db9202e22cc8a180213580b6670d7a7a7857
Karl Bielefeldt <karl.bielefeldt@adtran.com>
2017-12-20 09:41:21 -0600

    Use Array for coordinates instead of tuple.

--- 2017/20.scala
+++ 2017/20.scala
@@ -1,23 +1,23 @@
 import scala.io.Source
 import Math.abs
 
-type Coord = (Int, Int, Int)
+type Coord = Array[Int]
 case class Particle(i: Int, p: Coord, v: Coord, a: Coord) {
   //at^2 + bt + c = 0
   //(-b (+-) sqrt(b^2 - 4ac))/2a
   //bt + c = 0
   //t = -c/b
-  def collisions(other: Particle): List[Int] = ???
+  def collisions(other: Particle): Set[Int] = ???
 }
 
 object Particle {
   def apply(line: String, index: Int): Particle = {
-    val Array(p, v, a) = line.split(", ").map(_.drop(3).dropRight(1).split(",").map(_.toInt)).map{case Array(x, y, z) => (x, y, z)}
+    val Array(p, v, a) = line.split(", ").map(_.drop(3).dropRight(1).split(",").map(_.toInt))
     new Particle(index, p, v, a)
   }
 }
 
 val input = Source.fromFile("input20.txt").getLines.zipWithIndex.map{case (line, index) => Particle(line, index)}
 
-val answer1 = input.minBy{x => val a = x.a; abs(a._1) + abs(a._2) + abs(a._3)}.i
+val answer1 = input.minBy{_.a.map(abs).sum}.i
 println(answer1)
