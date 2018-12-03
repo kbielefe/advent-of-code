bfc1c259f07bd9205f49f02ec1a9a82c07f4a848
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2018-12-03 05:25:23 -0600

    Add 2018/3

---
+++ 2018/3.scala
@@ -0,0 +1,47 @@
+import scala.io.Source
+
+val input = Source.fromFile("input3.txt").getLines.toList
+
+case class Claim(id: Int, left: Int, top: Int, width: Int, height: Int)
+
+def parseClaim(in: String): Claim = {
+  val claim = """#(\d+)\s+@\s+(\d+),(\d+):\s+(\d+)x(\d+)""".r
+  in match {
+    case claim(id, left, top, width, height) => Claim(id.toInt, left.toInt, top.toInt, width.toInt, height.toInt)
+  }
+}
+
+val claims = input map parseClaim
+
+type ClaimMap = Map[(Int, Int), Set[Int]]
+
+def getClaimMap(claim: Claim): ClaimMap = {
+  val squares = for {
+    left <- (claim.left until (claim.left + claim.width))
+    top  <- (claim.top  until (claim.top  + claim.height))
+  } yield ((left, top) -> Set(claim.id))
+  squares.toMap
+}
+
+def mergeMaps(first: ClaimMap, second: ClaimMap): ClaimMap = {
+  second.foldLeft(first){case (accum, (nextIndex, nextId)) =>
+    val existing: Set[Int] = accum.getOrElse(nextIndex, Set.empty[Int])
+    accum + ((nextIndex, (nextId ++ existing)))
+  }
+}
+
+val claimMaps = claims map getClaimMap
+
+val mergedMaps = claimMaps reduce mergeMaps
+
+val overlappingClaims = mergedMaps filter {_._2.size > 1}
+
+val answer1 = overlappingClaims.size
+
+println(answer1)
+
+val overlappingIds = overlappingClaims.flatMap{_._2}.toSet
+val allIds = mergedMaps.flatMap{_._2}.toSet
+
+val answer2 = (allIds -- overlappingIds).head
+println(answer2)
