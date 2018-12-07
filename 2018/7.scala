f4ecc04bbafda46bb0dcc3ffb8cdd0c87f257502
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2018-12-07 00:39:52 -0600

    Complete part 2

--- 2018/7.scala
+++ 2018/7.scala
@@ -1,7 +1,11 @@
 import scala.io.Source
 import scala.annotation.tailrec
 
-val input = Source.fromFile("input7.txt").getLines.toList
+val example = false
+val input = if (example)
+    Source.fromFile("input7example.txt").getLines.toList
+  else
+    Source.fromFile("input7.txt").getLines.toList
 
 type Order = (Char, Char)
 
@@ -43,3 +47,31 @@ def toposort(result: List[Char], noIncoming: Set[Char], edges: Map[Char, Set[Cha
 
 val answer1 = toposort(List.empty[Char], getNoIncoming(edges, nodes), edges, nodes)
 println(answer1)
+
+val offset = if (example) 1 else 61
+def duration(task: Char): Int = task - 'A' + offset
+val workerCount = if (example) 2 else 5 
+
+@tailrec
+def parallelToposort(result: Int, noIncoming: Set[Char], edges: Map[Char, Set[Char]], nodes: Set[Char], wip: Set[(Char, Int)]): Int = {
+  if (noIncoming.isEmpty)
+    result - 1
+  else {
+    val newResult = result + 1
+    val (carryoverWip, completed) = wip map {case (task, remaining) => (task, remaining - 1)} partition {_._2 > 0}
+    val completedTasks = completed map {_._1}
+    val carryoverTasks = carryoverWip map {_._1}
+    val newNodes = nodes -- completedTasks
+    val newEdges = edges -- completedTasks
+    val newNoIncoming = getNoIncoming(newEdges, newNodes)
+
+    val availableWorkers = workerCount - carryoverWip.size
+    val newTasks = (newNoIncoming -- carryoverTasks).toList.sorted.take(availableWorkers).toSet
+
+    val newWip = carryoverWip ++ (newTasks map {task => (task, duration(task))})
+    parallelToposort(newResult, newNoIncoming, newEdges, newNodes, newWip)
+  }
+}
+
+val answer2 = parallelToposort(0, getNoIncoming(edges, nodes), edges, nodes, Set.empty[(Char, Int)])
+println(answer2)
