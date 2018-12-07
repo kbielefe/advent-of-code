6a2a1560a864333815461e2bd36cb5e806dded47
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2018-12-07 15:33:20 -0600

    Allow specifying of year and day in runner.

--- src/main/scala/common/Runner.scala
+++ src/main/scala/common/Runner.scala
@@ -3,8 +3,23 @@ import scala.io.Source
 
 object Runner {
   def main(args: Array[String]): Unit = {
-    val source = Source.fromResource("2018/1.txt")
-    val day = new advent2018.Day1(source)
+    if (args.size < 2) {
+      println("Call using year and day, i.e. 2018 1")
+      return
+    }
+    val date = s"${args(0)}/${args(1)}"
+    val source = Source.fromResource(s"${date}.txt")
+    val day = date match {
+      case "2018/1" => new advent2018.Day1(source)
+      case "2018/2" => new advent2018.Day2(source)
+      case "2018/3" => new advent2018.Day3(source)
+      case "2018/4" => new advent2018.Day4(source)
+      case "2018/5" => new advent2018.Day5(source)
+      case "2018/6" => new advent2018.Day6(source)
+      case "2018/7" => new advent2018.Day7(source)
+      case _        => println("Puzzle not available"); return
+    }
     println(day.answer1)
+    println(day.answer2)
   }
 }
