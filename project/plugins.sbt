cbcd097e6b35a9b4e7827f99e8e72a6c1f799e83
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2018-12-07 13:25:05 -0600

    Add sbt scaffold.

---
+++ project/plugins.sbt
@@ -0,0 +1,2 @@
+addSbtPlugin("com.artima.supersafe" % "sbtplugin" % "1.1.6")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
---
+++ src/test/scala/MainTests.scala
@@ -0,0 +1,11 @@
+import org.scalatest._
+
+class MainTests extends UnitSpec {
+  "Main" when {
+    "initialized" should {
+      "do something" in {
+        assert(true)
+      }
+    }
+  }
+}
