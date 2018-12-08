18a3ebd94454ddadabc62e178d8afb7ef21be98d
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2018-12-08 12:01:56 -0600

    Refactor 2018/8

--- src/main/scala/2018/8.scala
+++ src/main/scala/2018/8.scala
@@ -7,7 +7,16 @@ class Day8(source: Source) extends Day {
 
   val input = source.mkString.trim.split(" ").map{_.toInt}.toList
 
-  case class Node(children: List[Node], metadata: List[Int])
+  case class Node(children: List[Node], metadata: List[Int]) {
+    def metadataSum: Int = metadata.sum + children.map(_.metadataSum).sum
+
+    def value: Int =
+      if (children.isEmpty)
+        metadataSum
+      else {
+        metadata.filter{_ <= children.size}.map{n => children(n - 1).value}.sum
+      }
+  }
 
   val nextInt: State[List[Int], Int] = State(input => (input.tail, input.head))
   def nextNode: State[List[Int], Node] = for {
@@ -19,23 +28,6 @@ class Day8(source: Source) extends Day {
 
   def parseNode(input: List[Int]): Node = nextNode.runA(input).value
 
-  def sumMetadata(node: Node): Int = {
-    node.metadata.sum + node.children.map(sumMetadata).sum
-  }
-
-  def nodeValue(node: Node): Int = {
-    if (node.children.isEmpty)
-      sumMetadata(node)
-    else {
-      node.metadata.map{n => if (n > node.children.size) 0 else nodeValue(node.children(n - 1))}.sum
-    }
-  }
-
-  override def answer1: String = {
-    sumMetadata(parseNode(input)).toString
-  }
-
-  override def answer2: String = {
-    nodeValue(parseNode(input)).toString
-  }
+  override def answer1: String = parseNode(input).metadataSum.toString
+  override def answer2: String = parseNode(input).value.toString
 }
