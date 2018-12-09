51c1f102743108946506b5c6caa635e4d8c93813
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2018-12-09 17:51:42 -0600

    Refactor LinkedCircular into own file.

--- src/main/scala/2018/9.scala
+++ src/main/scala/2018/9.scala
@@ -1,43 +1,6 @@
 package advent2018
-import common.Day
+import common.{Day, LinkedCircular}
 import scala.io.Source
-
-// g maps node -> (back, forward)
-case class LinkedCircular[A](protected val g: Map[A, (A, A)]) {
-
-  def move(from: A, count: Int): A = {
-    if (count == 0) {
-      from
-    } else if (count < 0) {
-      Iterator.iterate(from){g(_)._1}.drop(-1 * count).next
-    } else {
-      Iterator.iterate(from){g(_)._2}.drop(count).next
-    }
-  }
-
-  def insertAfter(pred: A, inserted: A): LinkedCircular[A] = {
-    val (predBack, succ) = g(pred)
-    val (_, succForward) = g(succ)
-    if (pred == succ)
-      LinkedCircular(g + (pred -> (inserted, inserted)) + (inserted -> (pred, succ)))
-    else
-      LinkedCircular(g + (pred -> (predBack, inserted)) + (inserted -> (pred, succ)) + (succ -> (inserted, succForward)))
-  }
-
-  def delete(deleted: A): LinkedCircular[A] = {
-    val (pred, succ) = g(deleted)
-    val (predBack, _) = g(pred)
-    val (_, succForward) = g(succ)
-    if (pred == succ)
-      LinkedCircular(g - deleted + (pred -> (pred, succ)))
-    else
-      LinkedCircular(g - deleted + (pred -> (predBack, succ)) + (succ -> (pred, succForward)))
-  }
-
-  def toList(startNode: A): List[A] = {
-    Iterator.iterate(startNode){g(_)._2}.take(g.size).toList
-  }
-}
 
 class Day9(source: Source) extends Day {
 
--- src/test/scala/2018/9.scala
+++ src/test/scala/2018/9.scala
@@ -56,40 +56,10 @@ class TestDay9 extends UnitSpec {
 
   "answer 2" when {
     "given example" should {
-      "be ???" in {
-        val day = new Day9(example(9, 32))
-        day.answer2 shouldBe ""
+      "be 22563" in {
+        val day = new Day9(example(9, 25))
+        day.answer2 shouldBe "22563"
       }
     }
   }
-
-  /*
-  "moves" when {
-    val day = new Day9(example(0, 0))
-    "not a multiple of 23" should {
-      "skip one each time" in {
-        day.moves.take(10).toList shouldBe List(
-          (0, 0, 0, LinkedCircular(0~>0)),
-          (1, 1, 0, LinkedCircular(0~>1, 1~>0)),
-          (2, 2, 0, LinkedCircular(0~>2, 2~>1, 1~>0)),
-          (3, 3, 0, LinkedCircular(0~>2, 2~>1, 1~>3, 3~>0)),
-          (4, 4, 0, LinkedCircular(0~>4, 4~>2, 2~>1, 1~>3, 3~>0)),
-          (5, 5, 0, LinkedCircular(0~>4, 4~>2, 2~>5, 5~>1, 1~>3, 3~>0)),
-          (6, 6, 0, LinkedCircular(0~>4, 4~>2, 2~>5, 5~>1, 1~>6, 6~>3, 3~>0)),
-          (7, 7, 0, LinkedCircular(0~>4, 4~>2, 2~>5, 5~>1, 1~>6, 6~>3, 3~>7, 7~>0)),
-          (8, 8, 0, LinkedCircular(0~>8, 8~>4, 4~>2, 2~>5, 5~>1, 1~>6, 6~>3, 3~>7, 7~>0)),
-          (9, 9, 0, LinkedCircular(0~>8, 8~>4, 4~>9, 9~>2, 2~>5, 5~>1, 1~>6, 6~>3, 3~>7, 7~>0)))
-      }
-    }
-
-    "a multiple of 23" should {
-      "replace counterclockwise 7" in {
-        day.moves.drop(22).take(3).toList shouldBe List(
-          (22, 22, 0,  Graph(0~>16, 16~>8, 8~>17, 17~>4, 4~>18, 18~>9, 9~>19, 19~>2, 2~>20, 20~>10, 10~>21, 21~>5, 5~>22, 22~>11, 11~>1, 1~>12, 12~>6, 6~>13, 13~>3, 3~>14, 14~>7, 7~>15, 15~>0)),
-          (19, 23, 32, Graph(0~>16, 16~>8, 8~>17, 17~>4, 4~>18, 18~>19, 19~>2, 2~>20, 20~>10, 10~>21, 21~>5, 5~>22, 22~>11, 11~>1, 1~>12, 12~>6, 6~>13, 13~>3, 3~>14, 14~>7, 7~>15, 15~>0)),
-          (24, 24, 0,  Graph(0~>16, 16~>8, 8~>17, 17~>4, 4~>18, 18~>19, 19~>2, 2~>24, 24~>20, 20~>10, 10~>21, 21~>5, 5~>22, 22~>11, 11~>1, 1~>12, 12~>6, 6~>13, 13~>3, 3~>14, 14~>7, 7~>15, 15~>0)))
-      }
-    }
-  }
-  */
 }
