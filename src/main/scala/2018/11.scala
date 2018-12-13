82735998a0b03bd151812915e414e64001cdcabc
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2018-12-13 08:27:21 -0600

    Attempt to parallelize 2018/11

--- src/main/scala/2018/11.scala
+++ src/main/scala/2018/11.scala
@@ -1,6 +1,11 @@
 package advent2018
 import common.{Day, Dynamic, Visualize}
 import scala.io.Source
+import monix.tail.Iterant
+import monix.eval.Task
+import cats.implicits._
+import monix.execution.Scheduler.Implicits.global
+import scala.concurrent.duration._
 
 class Day11(source: Source) extends Day {
 
@@ -27,15 +32,17 @@ class Day11(source: Source) extends Day {
     ((x, y, size), power)
   }
 
-  def answer(sizes: Iterator[Int]): String = {
-    val gridPowers = for {
+  def answer(sizes: Iterant[Task, Int]): String = {
+    val gridPowers: Iterant[Task, ((Int, Int, Int), Int)] = for {
       size <- sizes
-      x    <- (1 to (301 - size)).iterator
-      y    <- (1 to (301 - size)).iterator
+      x    <- Iterant[Task].range(1, 302 - size)
+      y    <- Iterant[Task].range(1, 302 - size)
     } yield gridPower(x, y, size)
-    gridPowers.maxBy{_._2}._1.toString
+    val buffered = gridPowers.bufferTumbling(8).mapEval(b => Task.gather(b map {x => Task(x)}))
+    val max = gridPowers.maxByL{_._2}.map{_.get._1.toString}
+    max.runSyncUnsafe()
   }
 
-  override def answer1: String = answer(Iterator(3))
-  override def answer2: String = answer((1 to 300).iterator)
+  override def answer1: String = answer(Iterant[Task].of(3))
+  override def answer2: String = answer(Iterant[Task].range(1, 301))
 }
