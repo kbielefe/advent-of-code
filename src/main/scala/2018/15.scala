cf507a601f189d2de5863f445e7cfea60567b546
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2018-12-14 23:34:48 -0600

    Stubs for 2018/15

---
+++ src/main/scala/2018/15.scala
@@ -0,0 +1,8 @@
+package advent2018
+import common.Day
+import scala.io.Source
+
+class Day15(source: Source) extends Day {
+  override def answer1: String = ???
+  override def answer2: String = ???
+}
---
+++ src/test/scala/2018/15.scala
@@ -0,0 +1,14 @@
+package advent2018
+import org.scalatest._
+import common.UnitSpec
+import scala.io.Source
+
+class TestDay15 extends UnitSpec {
+  "something" when {
+    "given something" should {
+      "be something" in {
+        assert(true)
+      }
+    }
+  }
+}
