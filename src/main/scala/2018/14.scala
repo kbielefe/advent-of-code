ae6b58add951dd894a735d265aadf41da3761475
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2018-12-14 19:08:10 -0600

    Finish 2018/14 part 2.

--- src/main/scala/2018/14.scala
+++ src/main/scala/2018/14.scala
@@ -6,36 +6,66 @@ import monix.tail.Iterant
 import monix.eval.Coeval
 
 class Day14(source: Source) extends Day {
-  val input = source.mkString.trim.toInt
+  val input = source.mkString.trim
 
-  case class Scoreboard(size: Int = 0, recipeScores: Map[Int, Int] = Map.empty[Int, Int]) {
-    def getScore(recipe: Int): Int = recipeScores(recipe)
-    def append(score: Int): Scoreboard = Scoreboard(size + 1, recipeScores + (size -> score))
+  case class Scoreboard(size: Int = 0, recipeScores: Vector[Int] = Vector.empty) {
+    def getScore(recipe: Int): Int = if (recipe < 0) -1 else recipeScores(recipe)
+
+    def append(score: Int): Scoreboard = Scoreboard(size + 1, recipeScores :+ score)
+
     def moveRight(from: Int): Int = (from + getScore(from) + 1) % size
+
+    def patternPosition(pattern: String): Option[Int] = {
+      val patternSize = pattern.size
+      def existsAtOffset(offset: Int) = 
+        pattern.iterator.zipWithIndex.forall{case (p, i) => p.asDigit == getScore(size - patternSize + i - offset)}
+
+      if (existsAtOffset(0)) {
+        Some(size - pattern.size)
+      } else if (existsAtOffset(1)) {
+        Some(size - pattern.size - 1)
+      } else {
+        None
+      }
+    }
+
+    override def toString: String = recipeScores.mkString("Scoreboard(", ", ", ")")
+  }
+
+  object Scoreboard {
+    def apply(elems: Int*): Scoreboard = {
+      val vector = elems.toVector
+      Scoreboard(vector.size, vector)
+    }
   }
 
   def combineRecipes(x: Int, y: Int): Seq[Int] = (x + y).digits
 
-  def rounds: Iterant[Coeval, (Scoreboard, Int, Int)] = {
+  def rounds: Iterant[Coeval, Scoreboard] = {
     val initialScoreboard = Scoreboard().append(3).append(7)
     val initialState = (initialScoreboard, 0, 1)
     def nextState(prevState: (Scoreboard, Int, Int)) = {
       val (scoreboard, elf1Pos, elf2Pos) = prevState
       val newRecipes = combineRecipes(scoreboard.getScore(elf1Pos), scoreboard.getScore(elf2Pos))
-      val newScoreboard = newRecipes.foldLeft(scoreboard){_.append(_)}
+      val newScoreboard = newRecipes.foldLeft(scoreboard){_ append _}
       val newElf1Pos = newScoreboard.moveRight(elf1Pos)
       val newElf2Pos = newScoreboard.moveRight(elf2Pos)
       val newState = (newScoreboard, newElf1Pos, newElf2Pos)
-      (newState, newState)
+      (newScoreboard, newState)
     }
-    Iterant.fromStateAction[Coeval, (Scoreboard, Int, Int), (Scoreboard, Int, Int)](nextState)(initialState)
+    Iterant.fromStateAction[Coeval, (Scoreboard, Int, Int), Scoreboard](nextState)(initialState)
   }
 
   def score10After(recipeCount: Int): String = {
-    val finalScoreboard = rounds.map{_._1}.dropWhile{_.size < (recipeCount + 10)}.headOptionL.apply().get
+    val finalScoreboard = rounds.dropWhile{_.size < (recipeCount + 10)}.headOptionL.apply().get
     ((recipeCount) to (recipeCount + 9)).map{finalScoreboard.getScore(_)}.mkString
   }
 
-  override def answer1: String = score10After(input)
-  override def answer2: String = ???
+  def patternPosition(pattern: String): Int = {
+    rounds.map{_.patternPosition(pattern)}.dropWhile{!_.isDefined}.headOptionL.apply().get.get
+  }
+
+  override def answer1: String = score10After(input.toInt)
+  override def answer2: String = patternPosition(input).toString
+  // 10445031 is too low
 }
--- src/test/scala/2018/14.scala
+++ src/test/scala/2018/14.scala
@@ -30,4 +30,53 @@ class TestDay14 extends UnitSpec {
       }
     }
   }
+
+  "scoreboard.patternPosition" when {
+    "given 123456789 and searching for 6789" should {
+      "be 5" in {
+        val scoreboard = day.Scoreboard(1, 2, 3, 4, 5, 6, 7, 8, 9)
+        scoreboard.patternPosition("6789") shouldBe Some(5)
+      }
+    }
+
+    "given 123456789 and searching for 678" should {
+      "be 5" in {
+        val scoreboard = day.Scoreboard(1, 2, 3, 4, 5, 6, 7, 8, 9)
+        scoreboard.patternPosition("678") shouldBe Some(5)
+      }
+    }
+
+    "given 3, 7, 1, 0, 1, 0, 1, 2, 4, 5 and searching for 01245" should {
+      "be 5" in {
+        val scoreboard = day.Scoreboard(3, 7, 1, 0, 1, 0, 1, 2, 4, 5)
+        scoreboard.patternPosition("01245") shouldBe Some(5)
+      }
+    }
+  }
+
+  "patternPosition" when {
+    "given 51589" should {
+      "be 9" in {
+        day.patternPosition("51589") shouldBe 9
+      }
+    }
+
+    "given 01245" should {
+      "be 5" in {
+        day.patternPosition("01245") shouldBe 5
+      }
+    }
+
+    "given 92510" should {
+      "be 18" in {
+        day.patternPosition("92510") shouldBe 18 
+      }
+    }
+
+    "given 59414" should {
+      "be 2018" in {
+        day.patternPosition("59414") shouldBe 2018
+      }
+    }
+  }
 }
