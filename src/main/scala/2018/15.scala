3a076445eacbe8c36185deef118d7a4050322f6b
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2018-12-16 17:31:01 -0600

    2018/15 WIP

--- src/main/scala/2018/15.scala
+++ src/main/scala/2018/15.scala
@@ -1,8 +1,25 @@
 package advent2018
-import common.Day
+import common.{Day, Grid}
 import scala.io.Source
 
 class Day15(source: Source) extends Day {
-  override def answer1: String = ???
+  abstract class Cell(gridChar: Char) extends Grid.Cell {
+    override def char = gridChar
+  }
+  abstract class Creature(char: Char, attack: Int, hitPoints: Int) extends Cell(char)
+
+  case class Wall() extends Cell('#')
+  case class Elf(attack:    Int = 3, hitPoints: Int = 200) extends Creature('E', attack, hitPoints)
+  case class Goblin(attack: Int = 3, hitPoints: Int = 200) extends Creature('G', attack, hitPoints)
+
+  def charToCell(char: Char): Cell = char match {
+    case '#' => Wall()
+    case 'E' => Elf()
+    case 'G' => Goblin()
+  }
+
+  lazy val grid = Grid(0, 0, '.', source, _ => None, charToCell _)
+
+  override def answer1: String = grid.getLines().mkString("\n")
   override def answer2: String = ???
 }
