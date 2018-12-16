81373d4412f9d361367044f88396004512eb6884
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2018-12-16 09:53:48 -0600

    Add stubs for 2018/16

---
+++ src/main/scala/2018/16.scala
@@ -0,0 +1,8 @@
+package advent2018
+import common.Day
+import scala.io.Source
+
+class Day16(source: Source) extends Day {
+  override def answer1: String = ???
+  override def answer2: String = ???
+}
