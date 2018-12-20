af808b7253904b8a186c0018b1837642a0eef14d
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2018-12-20 16:41:34 -0600

    Finish 2018/20/1

--- src/main/scala/2018/20.scala
+++ src/main/scala/2018/20.scala
@@ -10,10 +10,44 @@ import scala.annotation.tailrec
 class Day20(source: Source) extends Day {
   type Point = (Int, Int)
 
-  sealed trait Regex
-  case class Move(direction: Char)           extends Regex
-  case class Branch(children: List[Regex])   extends Regex
-  case class Sequence(children: List[Regex]) extends Regex
+  // TODO: make a generic Tree class that can read and traverse directly from the string
+  sealed trait Regex {
+    def traverse(endpoints: Set[Point], paths: Map[Point, (Point, Int)]): (Set[Point], Map[Point, (Point, Int)])
+  }
+
+  case class Move(direction: Char)           extends Regex {
+    override def traverse(endpoints: Set[Point], paths: Map[Point, (Point, Int)]): (Set[Point], Map[Point, (Point, Int)]) = {
+      def newPoint(p: Point): Point = (p, direction) match {
+        case ((x, y), 'N') => (x, y - 1)
+        case ((x, y), 'S') => (x, y + 1)
+        case ((x, y), 'E') => (x + 1, y)
+        case ((x, y), 'W') => (x - 1, y)
+        case ((x, y),   _) => (x , y)
+      }
+      val newEndpoints = endpoints map newPoint
+      val newPaths = endpoints map {p =>
+        val length = paths.get(p).map(_._2).getOrElse(0) + 1
+        val alreadyExistingPath = paths.get(newPoint(p))
+        alreadyExistingPath.map{case (existingParent, existingLength) => if (length < existingLength) (newPoint(p), (p, length)) else (newPoint(p), (existingParent, existingLength))} getOrElse (newPoint(p), (p, length))
+      }
+      (newEndpoints, paths ++ newPaths)
+    }
+  }
+
+  case class Branch(children: List[Regex])   extends Regex {
+    override def traverse(endpoints: Set[Point], paths: Map[Point, (Point, Int)]): (Set[Point], Map[Point, (Point, Int)]) = {
+      val list = children.map{child => child.traverse(endpoints, paths)}
+      val newEndpoints = list.flatMap{_._1}.toSet
+      val newPaths = list.flatMap{_._2}.toMap
+      (newEndpoints, newPaths)
+    }
+  }
+
+  case class Sequence(children: List[Regex]) extends Regex {
+    override def traverse(endpoints: Set[Point], paths: Map[Point, (Point, Int)]): (Set[Point], Map[Point, (Point, Int)]) = {
+      children.foldLeft((endpoints, paths)){case ((endpoints, paths), child) => child.traverse(endpoints, paths)}
+    }
+  }
 
   final def parseRegex(regex: String): Regex = {
     parseSequence(regex drop 1)._2
@@ -24,8 +58,10 @@ class Day20(source: Source) extends Day {
   }
 
   final def parseBranch(regex: String, accum: List[Regex] = List.empty[Regex]): (String, Branch) = {
-    if (regex.isEmpty || (")$" contains regex.head)) {
+    if (regex.isEmpty) {
       (regex, Branch(accum.reverse))
+    } else if (")$" contains regex.head) {
+      (regex.tail, Branch(accum.reverse))
     } else {
       val (nextRegex, nextElement) = regex.head match {
         case 'N' => parseSequence(regex)
@@ -55,6 +91,11 @@ class Day20(source: Source) extends Day {
     }
   }
 
-  override def answer1: String = ???
+  def furthestRoomDistance(regex: String): Int = {
+    val map = parseRegex(regex).traverse(Set((0, 0)), Map.empty)._2
+    map.map(_._2._2).max
+  }
+
+  override def answer1: String = furthestRoomDistance(source.mkString.trim).toString
   override def answer2: String = ???
 }
--- src/test/scala/2018/20.scala
+++ src/test/scala/2018/20.scala
@@ -37,11 +37,85 @@ class TestDay20 extends UnitSpec {
       }
     }
 
+    "given a regex with a move after an empty branch" should {
+      "include the last move" in {
+        parseRegex("^N(E|)W$") shouldBe Sequence(List(Move('N'), Branch(List(Sequence(List(Move('E'))), Sequence(List()))), Move('W')))
+      }
+    }
     "given the real input" should {
       "not overflow the stack" in {
         val regex = parseRegex(Source.fromResource("2018/20.txt").mkString.trim)
-        println(regex)
         assert(true)
+      }
+    }
+  }
+
+  "Move.traverse" when {
+    "traversing a single-element set of endpoints" should {
+      "return those endpoints moved, and a path of length 1" in {
+        val (endpoints, paths) = Move('N').traverse(Set((0, 0)), Map.empty)
+        endpoints shouldBe Set((0, -1))
+        paths shouldBe Map(((0, -1) -> ((0, 0), 1)))
+      }
+    }
+  }
+
+  "Sequence.traverse" when {
+    "traversing a single-element set of endpoints" should {
+      "return those endpoints moved, and a path of length 2" in {
+        val (endpoints, paths) = Sequence(List(Move('N'), Move('E'))).traverse(Set((0, 0)), Map.empty)
+        endpoints shouldBe Set((1, -1))
+        paths shouldBe Map(((0, -1) -> ((0, 0), 1)), ((1, -1) -> ((0, -1), 2)))
+      }
+    }
+
+    "traversing an empty sequence" should {
+      "return everything unchanged" in {
+        val (endpoints, paths) = Sequence(List()).traverse(Set((0, 0)), Map.empty)
+        endpoints shouldBe Set((0, 0))
+        paths shouldBe empty
+      }
+    }
+  }
+
+  "Branch.traverse" when {
+    "traversing a single-element set of endpoints" should {
+      "return that endpoint split and  moved, and paths of length 1" in {
+        val (endpoints, paths) = Branch(List(Move('N'), Move('E'))).traverse(Set((0, 0)), Map.empty)
+        endpoints shouldBe Set((0, -1), (1, 0))
+        paths shouldBe Map(((0, -1) -> ((0, 0), 1)), ((1, 0) -> ((0, 0), 1)))
+      }
+    }
+
+    "branch has an empty sequence" should {
+      "return one split of length 1 and one of length 0" in {
+        val (endpoints, paths) = Branch(List(Move('N'), Sequence(List()))).traverse(Set((0, 0)), Map.empty)
+        endpoints shouldBe Set((0, 0), (0, -1))
+        paths shouldBe Map(((0, -1) -> ((0, 0), 1)))
+      }
+    }
+  }
+
+  "Regex.traverse" when {
+    "containing empty branches" should {
+      "handle it correctly" in {
+        val regex = parseRegex("^N(E|)W$")
+        println(regex)
+        val (endpoints, paths) = regex.traverse(Set((0, 0)), Map.empty)
+        endpoints shouldBe Set((0, -1), (-1, -1))
+        paths shouldBe Map(((0, -1) -> ((0, 0), 1)), ((1, -1) -> ((0, -1), 2)), ((-1, -1) -> ((0, -1), 2)))
+      }
+    }
+  }
+  "furthestRoomDistance" when {
+    "given examples" should {
+      "give correct results" in {
+        furthestRoomDistance("^WNE$") shouldBe 3
+        furthestRoomDistance("^ENWWW(NEEE|SSE(EE|N))$") shouldBe 10
+        furthestRoomDistance("^N(E|)W$") shouldBe 2
+        furthestRoomDistance("^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$") shouldBe 18
+        furthestRoomDistance("^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$") shouldBe 23
+        furthestRoomDistance("^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$") shouldBe 31
       }
     }
   }
