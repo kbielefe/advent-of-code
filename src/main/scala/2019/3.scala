b6af4bd7c0b83e527e8fc191ba76c384f9949bba
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2019-12-03 12:26:41 -0600

    2019 3 part 2 WIP

--- src/main/scala/2019/3.scala
+++ src/main/scala/2019/3.scala
@@ -3,35 +3,31 @@ import common.Day
 import scala.io.Source
 
 class Day3(source: Source) extends Day {
-  val input = source.getLines.map(_.split(",").map(x => (x.head, x.tail.toInt))).toVector
-
-  def segmentSquares(x: Int, y: Int, direction: Char, distance: Int): Set[(Int, Int)] = direction match {
-    case 'U' => ((y - distance) to y).map(y => ((x, y))).toSet
-    case 'D' => (y to (y + distance)).map(y => ((x, y))).toSet
-    case 'L' => ((x - distance) to x).map(x => ((x, y))).toSet
-    case 'R' => (x to (x + distance)).map(x => ((x, y))).toSet
-  }
+  val input = source.getLines.map(_.split(",").map(x => (x.head, x.tail.toInt)).toList).toVector
 
   @scala.annotation.tailrec
-  private def wireSquares(directions: Array[(Char, Int)], acc: Set[(Int, Int)] = Set.empty, x: Int = 0, y: Int = 0): Set[(Int, Int)] = {
+  private def path(directions: List[(Char, Int)], x: Int = 0, y: Int = 0, acc: List[(Int, Int)] = List.empty): List[(Int, Int)] = {
     if (directions.isEmpty) {
       acc
     } else {
-      val (direction, distance) = directions.head
-      val (newX, newY) = direction match {
-        case 'U' => (x, y - distance)
-        case 'D' => (x, y + distance)
-        case 'L' => (x - distance, y)
-        case 'R' => (x + distance, y)
+      val ((direction, count) :: tail) = directions
+      if (count == 0) {
+        path(tail, x, y, acc)
+      } else {
+        direction match {
+          case 'U' => path((direction, count - 1) :: tail, x, y - 1, (x, y) :: acc)
+          case 'D' => path((direction, count - 1) :: tail, x, y + 1, (x, y) :: acc)
+          case 'L' => path((direction, count - 1) :: tail, x - 1, y, (x, y) :: acc)
+          case 'R' => path((direction, count - 1) :: tail, x + 1, y, (x, y) :: acc)
+        }
       }
-      wireSquares(directions.tail, acc ++ segmentSquares(x, y, direction, distance), newX, newY)
     }
   }
 
-
-  val closestIntersection = (wireSquares(input(0)).intersect(wireSquares(input(1))) - ((0, 0))).map{case (x, y) => math.abs(x) + math.abs(y)}.min
+  val closestIntersection = (path(input(0)).toSet.intersect(path(input(1)).toSet) - ((0, 0))).map{case (x, y) => math.abs(x) + math.abs(y)}.min
 
   override def answer1 = closestIntersection.toString
 
-  override def answer2 = "unimplemented"
+  override def answer2 = (path(input(0)).zipWithIndex.drop(1) ++ path(input(1)).zipWithIndex.drop(1)).groupBy{case ((x, y), index) => (x, y)}.filter(_._2.size > 1).map{_._2.map{_._2}.sum}.min.toString
+  // 4142 is too low
 }
