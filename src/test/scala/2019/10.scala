c7d74006c6bc4b6fee98326509168652c135bbd0
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2019-12-10 16:18:23 -0600

    Simplify 2019/10 by factoring out grouping by exact angles.

--- src/test/scala/2019/10.scala
+++ src/test/scala/2019/10.scala
@@ -30,30 +30,5 @@ class TestDay10 extends UnitSpec {
         day.answer1 shouldBe "33"
       }
     }
-
-    "given specific blocking example" should {
-      "block specified values" in {
-        val input = Source.fromString(
-          """|#.........
-             |...#......
-             |...#..#...
-             |.####....#
-             |..#.#.#...
-             |.....#....
-             |..###.#.##
-             |.......#..
-             |....#...#.
-             |...#..#..#""".stripMargin)
-
-        val day = new Day10(input)
-        val blockers = day.blockers(0, 0) _
-        blockers(3, 1) shouldBe Set((6, 2), (9, 3))
-        blockers(3, 2) shouldBe Set((6, 4), (9, 6))
-        blockers(3, 3) shouldBe Set((4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9))
-        blockers(2, 3) shouldBe Set((4, 6), (6, 9))
-        blockers(2, 4) shouldBe Set((3, 6), (4, 8))
-        blockers(4, 3) shouldBe Set((8, 6))
-      }
-    }
   }
 }
