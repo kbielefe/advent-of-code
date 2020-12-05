99516b00e56668847c587facc452e4126456b10f
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2020-12-04 23:21:13 -0600

    2020/5

--- src/main/scala/2020/5.scala
+++ src/main/scala/2020/5.scala
@@ -1,14 +1,28 @@
 package advent2020
 
+import cats.implicits._
 import common._
 import monix.eval.Task
 import monix.reactive.Observable
-import scala.util.Try
 
-object Day5 extends MapDay[Long, Long](2020, 5) {
-  override def part1(input: Observable[Map[String, String]]): Task[Long] =
-    ???
+object Day5 extends StringsDay[Int, Int](2020, 5) {
+  private def seatId(string: String): Int = {
+    val binary = string.map{
+      case 'L' => '0'
+      case 'R' => '1'
+      case 'F' => '0'
+      case 'B' => '1'
+    }
+    Integer.parseInt(binary, 2)
+  }
 
-  override def part2(input: Observable[Map[String, String]]): Task[Long] =
-    ???
+  override def part1(input: Observable[String]): Task[Int] =
+    input.map(seatId).maxL.map(_.get)
+
+  override def part2(input: Observable[String]): Task[Int] =
+    input.map(seatId).toListL.map(_.toSet).map{seats =>
+      val min = seats.min
+      val max = seats.max
+      ((min to max).toSet -- seats).head
+    }
 }
