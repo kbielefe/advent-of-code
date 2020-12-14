db09afa0ec4fc967540079eae9943e9d3ce95ffe
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2020-12-13 23:08:18 -0500

    Fix overflow of Long.

--- src/main/scala/2020/13.scala
+++ src/main/scala/2020/13.scala
@@ -2,7 +2,7 @@ package advent2020
 
 import common._
 
-object Day13 extends SyncStringsDay[Int, Long](2020, 13) {
+object Day13 extends SyncStringsDay[Int, BigInt](2020, 13) {
   override def part1(input: Seq[String]): Int = {
     val earliest = input(0).toInt
     val busIDs = input(1).split(",").filterNot(_ == "x").map(_.toInt)
@@ -10,22 +10,28 @@ object Day13 extends SyncStringsDay[Int, Long](2020, 13) {
     earliestBus * shortestWait
   }
 
-  override def part2(input: Seq[String]): Long = {
-    val busIDs = input(1).split(",").zipWithIndex.filterNot(_._1 == "x").map{case (id, index) => (id.toLong - index.toLong, id.toLong)}
-    val remainder = busIDs.reduceLeft[(Long, Long)]{case ((combinedA, combinedN), (nextA, nextN)) =>
+  override def part2(input: Seq[String]): BigInt = {
+    val busIDs = input(1)
+      .split(",")
+      .zipWithIndex
+      .filterNot(_._1 == "x")
+      .map{case (id, index) => (BigInt(id), BigInt(index))}
+      .map{case (id, index) => (id - index, id)}
+
+    val remainder = busIDs.reduceLeft[(BigInt, BigInt)]{case ((combinedA, combinedN), (nextA, nextN)) =>
       chineseRemainder(combinedA, combinedN, nextA, nextN)
     }
     remainder._1
   }
-  // 765,542,694,402,353 is too high
 
-  private def chineseRemainder(a1: Long, n1: Long, a2: Long, n2: Long): (Long, Long) = {
+  private def chineseRemainder(a1: BigInt, n1: BigInt, a2: BigInt, n2: BigInt): (BigInt, BigInt) = {
+    println(a1, n1, a2, n2)
     val (m1, m2) = bézout(n1, n2)
     normalize(a1 * m2 * n2 + a2 * m1 * n1, n1 * n2)
   }
 
-  private def bézout(a: Long, b: Long): (Long, Long) = {
-    def helper(oldR: Long, r: Long, oldS: Long, s: Long, oldT: Long, t: Long): (Long, Long) =
+  private def bézout(a: BigInt, b: BigInt): (BigInt, BigInt) = {
+    def helper(oldR: BigInt, r: BigInt, oldS: BigInt, s: BigInt, oldT: BigInt, t: BigInt): (BigInt, BigInt) =
       if (r == 0)
         (oldS, oldT)
       else {
@@ -35,7 +41,7 @@ object Day13 extends SyncStringsDay[Int, Long](2020, 13) {
     helper(a, b, 1, 0, 0, 1)
   }
 
-  private def normalize(a: Long, n: Long): (Long, Long) = {
+  private def normalize(a: BigInt, n: BigInt): (BigInt, BigInt) = {
     val newA = a % n
     if (newA < 0)
       (newA + n, n)
