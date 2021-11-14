99b17ba1c34ac6b413c846162eaded0543547d3f
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2021-11-14 15:16:26 -0600

    Copy answer to clipboard.

--- src/main/scala/common/Runner.scala
+++ src/main/scala/common/Runner.scala
@@ -1,21 +1,30 @@
+import java.awt.datatransfer.StringSelection
+import java.awt.Toolkit
 import scala.io.Source
 
 object Runner:
   @main def main(year: Int, day: Int, part: Int): Unit =
-    val run = runDay(year, day)
+    val input = Source.fromFile(s"input/$year/$day.txt").mkString
+    val run = runDay(input, part)
     (year, day) match
-      case (2020, 1) => run(advent2020.Day1.part1, advent2020.Day1.part2, part)
+      case (2020, 1) => run(advent2020.Day1.part1, advent2020.Day1.part2)
       case _         => println("Puzzle solution not found.")
 
-  private class runDay(year: Int, day: Int):
-    def apply[A: Read, B: Show, C: Read, D: Show](part1: A => B, part2: C => D, part: Int): Unit =
-      val input = Source.fromFile(s"input/$year/$day.txt").mkString
+  private class runDay(input: String, part: Int):
+    def apply[A: Read, B: Show, C: Read, D: Show](part1: A => B, part2: C => D): Unit =
       val start = System.currentTimeMillis()
-      val output = if part == 1 then
-        summon[Show[B]].show(part1(summon[Read[A]].read(input)))
-      else
-        summon[Show[D]].show(part2(summon[Read[C]].read(input)))
+      val output = if part == 1 then run(part1) else run(part2)
       val end = System.currentTimeMillis()
-      println(s"Year $year, Day $day, Part $part (${end - start} ms): $output")
+      copyToClipboard(output)
+      println(s"(${end - start} ms)\n$output")
+
+    private def run[A, B](f: A => B)(using read: Read[A], show: Show[B]): String =
+      show.show(f(read.read(input)))
+
+    private def copyToClipboard(text: String): Unit =
+      Toolkit
+        .getDefaultToolkit()
+        .getSystemClipboard()
+        .setContents(new StringSelection(text), null)
   end runDay
 end Runner
