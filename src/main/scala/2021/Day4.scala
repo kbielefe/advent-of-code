1b36b7c7a8c89fd8af3af77bc5b290687f017c30
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2021-12-04 11:10:55 -0600

    Use Set for marked.

--- src/main/scala/2021/Day4.scala
+++ src/main/scala/2021/Day4.scala
@@ -6,7 +6,7 @@ object Day4:
   given Read[Board] with
     def read(input: String): Board =
       val grid = summon[Read[Grid[Int]]].read(input)
-      Board(Map.empty, grid.map(_.swap), 0)
+      Board(Set.empty, grid.map(_.swap), 0)
 
   def part1(input: Header[List[Int], MultiLine[Board]]): Int =
     input.body.map(_.playUntilWins(input.header)).minBy(_._2)._1.score
@@ -14,18 +14,18 @@ object Day4:
   def part2(input: Header[List[Int], MultiLine[Board]]): Int =
     input.body.map(_.playUntilWins(input.header)).maxBy(_._2)._1.score
 
-  case class Board(marked: Map[Int, Pos], unmarked: Map[Int, Pos], lastCalled: Int):
+  case class Board(marked: Set[Pos], unmarked: Map[Int, Pos], lastCalled: Int):
     def mark(num: Int): Board =
       if unmarked.contains(num) then
-        Board(marked + (num -> unmarked(num)), unmarked - num, num)
+        Board(marked + unmarked(num), unmarked - num, num)
       else
         this
 
     def score: Int = unmarked.keys.sum * lastCalled
 
     def wins: Boolean =
-      val rows = marked.values.groupBy(_._1).exists(_._2.size == 5)
-      val cols = marked.values.groupBy(_._2).exists(_._2.size == 5)
+      val rows = marked.groupBy(_._1).exists(_._2.size == 5)
+      val cols = marked.groupBy(_._2).exists(_._2.size == 5)
       rows || cols
 
     def playUntilWins(calledNumbers: List[Int]): (Board, Int) =
