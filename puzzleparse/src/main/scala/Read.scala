b3a58a7ea8a5512a9a7d3ccf80715e256e92229b
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2021-12-07 17:30:00 -0600

    Pos tests pass.

--- puzzleparse/src/main/scala/Read.scala
+++ puzzleparse/src/main/scala/Read.scala
@@ -1,56 +1,54 @@
 package puzzleparse
 import scala.deriving.Mirror
-import scala.compiletime.erasedValue
+import scala.compiletime.{erasedValue, summonInline}
 import scala.reflect.ClassTag
 import scala.util.matching.Regex.Match
 
 trait Read[A]:
-  def read(input: String): A
-  def pattern: String
-  def extract(m: Match, group: Int): A
-
-given Read[Int] with
-  def read(input: String): Int =
-    pattern.r.findFirstIn(input).map(_.toInt).get
-  def pattern: String = "-?\\d+"
-  def extract(m: Match, group: Int): Int =
-    m.group(group).toInt
-
-given Read[String] with
-  def read(input: String): String =
-    input
-  def pattern: String = "(?s).*"
-  def extract(m: Match, group: Int): String =
-    m.group(group).trim
-
-given Read[EmptyTuple] with
-  def read(input: String): EmptyTuple = EmptyTuple
-  def pattern: String = ""
-  def extract(m: Match, group: Int): EmptyTuple = EmptyTuple
-
-given [H : Read, T <: Tuple : Read]: Read[H *: T] with
-  def read(input: String): H *: T =
+  def read(input: String): A =
     val m = pattern.r.findFirstMatchIn(input).get
     extract(m, 1)
+  def pattern: String
+  def extract(m: Match, group: Int): A
+  def groupCount: Int
 
+given Read[Int] with
+  def pattern: String = "(-?\\d+)"
+  def extract(m: Match, group: Int): Int =
+    m.group(group).toInt
+  def groupCount: Int = 1
+
+given Read[String] with
+  def pattern: String = "(?s)(.*)"
+  def extract(m: Match, group: Int): String =
+    m.group(group).trim
+  def groupCount: Int = 1
+
+given Read[EmptyTuple] with
+  override def read(input: String): EmptyTuple = EmptyTuple
+  def pattern: String = ""
+  def extract(m: Match, group: Int): EmptyTuple = EmptyTuple
+  def groupCount: Int = 0
+
+given [H : Read, T <: Tuple : Read]: Read[H *: T] with
   inline def pattern: String =
     val sep = inline erasedValue[T] match
       case _: EmptyTuple => ""
       case _ => ".*?"
-    "(" + summon[Read[H]].pattern + ")" + sep + summon[Read[T]].pattern
+    summon[Read[H]].pattern + sep + summon[Read[T]].pattern
 
   def extract(m: Match, group: Int): H *: T =
-    summon[Read[H]].extract(m, group) *: summon[Read[T]].extract(m, group + 1)
+    val headReader = summon[Read[H]]
+    headReader.extract(m, group) *: summon[Read[T]].extract(m, group + headReader.groupCount)
+
+  def groupCount: Int =
+    summon[Read[H]].groupCount + summon[Read[T]].groupCount
 
 object Read:
-  inline given derived[T](using m: Mirror.ProductOf[T]): Read[T] =
+  inline given derived[T](using mirror: Mirror.ProductOf[T]): Read[T] =
     new Read[T]:
-      def read(input: String): T =
-        m.fromProduct(fold[m.MirroredElemTypes](input))
-      def pattern: String = ???
-      def extract(m: Match, group: Int): T = ???
-
-  private inline def fold[T <: Tuple](input: String): Tuple =
-    inline erasedValue[T] match
-      case _: EmptyTuple => EmptyTuple
-      case _: (t *: ts)  => ???
+      val reader = summonInline[Read[mirror.MirroredElemTypes]]
+      override def read(input: String): T = mirror.fromProduct(reader.read(input))
+      def pattern: String = reader.pattern
+      def extract(m: Match, group: Int): T = mirror.fromProduct(reader.extract(m, group))
+      def groupCount: Int = reader.groupCount
