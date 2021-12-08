2f80a9ec392da0e7b85768751f2ccdc43bf5e5bf
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2021-12-08 12:34:59 -0600

    Remove need for intermediate mapping.

--- src/main/scala/2021/Day8.scala
+++ src/main/scala/2021/Day8.scala
@@ -1,38 +1,35 @@
 package advent2021
-import puzzleparse.{*, given}
+import puzzleparse.Letters
 
 object Day8:
   def part1(input: List[List[Letters]]): Int =
-    input.flatMap(_.drop(10)).map(_.size).count(Set(2,3,4,7).contains(_))
+    input.flatMap(_.drop(11)).map(_.size).count(Set(2,3,4,7) contains _)
 
   def part2(input: List[List[Letters]]): Int =
     input.map(unscramble).sum
 
   private def unscramble(input: List[Letters]): Int =
-    val inputs = input.take(10)
-    val outputs = input.drop(11) // 11 to remove the pipe
-    val one   = hasSize(inputs, 2)
-    val four  = hasSize(inputs, 4)
-    def findMapping(input: String): (String, Int) =
-      def size(other: Set[Char]): Int = (input.toSet & other).size
-      val key = (input.size, size(one), size(four))
-      (input.sorted -> overlaps(key))
-    val mapping = inputs.map(findMapping).toMap
-    outputs.map(_.sorted).map(mapping).foldLeft(0)((acc, digit) => acc * 10 + digit)
+    val inputs  = input.take(10)
+    val outputs = input.drop(11)
+    val one  = hasSize(inputs, 2)
+    val four = hasSize(inputs, 4)
+    outputs.map(segmentsToDigit(one, four)).foldLeft(0)(_ * 10 + _)
 
   private def hasSize(inputs: List[Letters], size: Int): Set[Char] =
     inputs.find(_.size == size).get.toSet
 
-  private val overlaps = Map(
-    // (size, overlaps with 1, overlaps with 4)
-    (6, 2, 3) -> 0,
-    (2, 2, 2) -> 1,
-    (5, 1, 2) -> 2,
-    (5, 2, 3) -> 3,
-    (4, 2, 4) -> 4,
-    (5, 1, 3) -> 5,
-    (6, 1, 3) -> 6,
-    (3, 2, 2) -> 7,
-    (7, 2, 4) -> 8,
-    (6, 2, 4) -> 9
-  )
+  private def overlapCount(other: Set[Char], segments: String): Int =
+    (segments.toSet & other).size
+
+  private def segmentsToDigit(one: Set[Char], four: Set[Char])(segments: String): Int =
+    (segments.size, overlapCount(one, segments), overlapCount(four, segments)) match
+      case (6, 2, 3) => 0
+      case (2, _, _) => 1
+      case (5, 1, 2) => 2
+      case (5, 2, 3) => 3
+      case (4, _, _) => 4
+      case (5, 1, 3) => 5
+      case (6, 1, 3) => 6
+      case (3, _, _) => 7
+      case (7, _, _) => 8
+      case (6, 2, 4) => 9
