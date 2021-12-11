7ff77b6250d94735575e0ec27d4c430e103daad3
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2021-12-11 00:19:58 -0600

    Implement 2021/11

--- src/main/scala/common/Types.scala
+++ src/main/scala/common/Types.scala
@@ -6,7 +6,7 @@ opaque type Binary <: Int = Int
 opaque type Letter <: Char = Char
 opaque type Letters <: String = String
 case class Pos(row: Int, col: Int)
-opaque type Grid[A] <: Map[Pos, A] = Map[Pos, A]
+opaque type Grid[+A] <: Map[Pos, A] = Map[Pos, A]
 opaque type MultiLine[A] <: List[A] = List[A]
 opaque type DMap[D <: String & Singleton, K, V] <: Map[K, V] = Map[K, V]
 opaque type DList[D <: String & Singleton, A] <: List[A] = List[A]
