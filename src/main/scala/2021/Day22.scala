dcae786010e2b02752ce8ffdca01d874f4c23384
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2021-12-24 13:38:25 -0600

    Efficient implementation of 2021/22 part 2.

--- src/main/scala/2021/Day22.scala
+++ src/main/scala/2021/Day22.scala
@@ -3,11 +3,11 @@ package advent2021
 object Day22:
   def part1(input: List[String]): Long =
     val cuboids = input.map(parseCuboid).takeWhile(_.x.start.abs <= 50)
-    buildExpression(cuboids).cardinality.evaluate
+    cuboids.tails.map(tail => buildExpression(tail).cardinality.evaluate).sum
 
   def part2(input: List[String]): Long =
     val cuboids = input.map(parseCuboid)
-    buildExpression(cuboids).cardinality.evaluate
+    cuboids.tails.map(tail => buildExpression(tail).cardinality.evaluate).sum
 
   def parseCuboid(input: String): Cuboid =
     val regex = """(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)""".r
@@ -15,8 +15,10 @@ object Day22:
       case regex(on, xmin, xmax, ymin, ymax, zmin, zmax) =>
         Cuboid(on == "on", xmin.toInt to xmax.toInt, ymin.toInt to ymax.toInt, zmin.toInt to zmax.toInt)
 
-  def buildExpression(cuboids: List[Cuboid]): Expr =
-    cuboids.foldLeft[Expr](Empty)((expr, cuboid) => if cuboid.on then Union(expr, cuboid) else Diff(expr, cuboid))
+  def buildExpression(cuboids: List[Cuboid]): Expr = cuboids match
+    case head :: tail if head.on =>
+      tail.filter(_.overlaps(head)).foldLeft[Expr](head)((expr, cuboid) => Diff(expr, cuboid))
+    case _ => Empty
 
   sealed trait Expr derives CanEqual:
     def cardinality: Cardinality
@@ -28,23 +30,19 @@ object Day22:
     def cardinality: Cardinality =
       Minus(left.cardinality, Intersection(left, right).cardinality)
 
-  case class Union(left: Expr, right: Cuboid) extends Expr:
-    def cardinality: Cardinality =
-      Minus(Plus(left.cardinality, right), Intersection(left, right).cardinality)
-
   case class Intersection(left: Expr, right: Cuboid) extends Expr:
     def cardinality: Cardinality = left match
-      case Empty => Zero
+      case Empty =>
+        Zero
+      case c: Cuboid =>
+        c & right
+      case Intersection(il, ir) =>
+        Intersection(il, ir & right).cardinality
       case Diff(dl, dr) =>
         Minus(Intersection(dl, right).cardinality, Intersection(Intersection(dl, dr), right).cardinality)
-      case Union(ul, ur) => Union(Intersection(ul, right), ur & right).cardinality
-      case Intersection(il, ir) => Intersection(il, ir & right).cardinality
 
   sealed trait Cardinality:
     def evaluate: Long
-
-  case class Plus(left: Cardinality, right: Cardinality) extends Cardinality:
-    def evaluate: Long = left.evaluate + right.evaluate
 
   case class Minus(left: Cardinality, right: Cardinality) extends Cardinality:
     def evaluate: Long = left.evaluate - right.evaluate
@@ -52,12 +50,15 @@ object Day22:
   case object Zero extends Cardinality:
     def evaluate: Long = 0
 
-  case class Cuboid(on: Boolean, x: Range, y: Range, z: Range) extends Cardinality:
-    override def toString: String =
-      val onOrOff = if on then "on " else "off"
-      f"$onOrOff $evaluate%3d ${x.start}%2d..${x.end}%2d, ${y.start}%2d..${y.end}%2d, ${z.start}%2d..${z.end}%2d"
-
+  case class Cuboid(on: Boolean, x: Range, y: Range, z: Range) extends Expr with Cardinality:
+    def cardinality: Cardinality = this
     def evaluate: Long = x.length.toLong * y.length * z.length
+
+    def overlaps(other: Cuboid): Boolean =
+      val newX = x.overlap(other.x)
+      val newY = y.overlap(other.y)
+      val newZ = z.overlap(other.z)
+      !(newX.isEmpty || newY.isEmpty || newZ.isEmpty)
 
     def &(other: Cuboid): Cuboid =
       val newX = x.overlap(other.x)
