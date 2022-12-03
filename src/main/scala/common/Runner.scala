ca1f1b15e0a25deec9a9b29469e327406c011962
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2022-12-02 23:17:08 -0600

    Solve 2022/3

--- src/main/scala/common/Runner.scala
+++ src/main/scala/common/Runner.scala
@@ -26,6 +26,7 @@ object Runner:
     (year, day) match
       case (2022, 1) => run(advent2022.Day1.part1, advent2022.Day1.part2)
       case (2022, 2) => run(advent2022.Day2.part1, advent2022.Day2.part2)
+      case (2022, 3) => run(advent2022.Day3.part1, advent2022.Day3.part2)
       case (2016, 1) => run(advent2016.Day1.part1, advent2016.Day1.part2)
       case (2016, 2) => run(advent2016.Day2.part1, advent2016.Day2.part2)
       case (2016, 3) => run(advent2016.Day3.part1, advent2016.Day3.part2)
