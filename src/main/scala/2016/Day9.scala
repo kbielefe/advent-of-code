5091e78db4611f857f776f6f22e80c72ec02cda3
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2022-12-05 21:18:09 -0600

    Solve 2016/9 part 1 with just size, not actual decompression

--- src/main/scala/2016/Day9.scala
+++ src/main/scala/2016/Day9.scala
@@ -1,27 +1,19 @@
 package advent2016
 
 object Day9:
-  def part1(input: String): Int =
-    decompress(input.trim.iterator).size
+  def part1(input: String): Long =
+    decompress(input.trim.iterator)
 
-  def part2(input: String): Int =
+  def part2(input: String): Long =
     ???
 
   private val markerRegex = """(\d+)x(\d+)""".r
 
-  private def decompress(input: Iterator[Char]): Iterator[Char] =
+  private def decompress(input: Iterator[Char]): Long =
     if input.hasNext then
       val (raw, markerPlus) = input.span(_ != '(')
       val (marker, remainder) = markerPlus.span(_ != ')')
       val markerRegex(count, repeat) = marker.drop(1).mkString: @unchecked
-      val (repeated, last) = remainder.drop(1).duplicate
-      raw ++ repeatIter(repeat.toInt, repeated.take(count.toInt)) ++ decompress(last.drop(count.toInt))
+      raw.size + (repeat.toLong * count.toLong) + decompress(remainder.drop(1 + count.toInt))
     else
-      Iterator.empty
-
-  private def repeatIter(count: Int, input: Iterator[Char]): Iterator[Char] =
-    if count == 0 then
-      Iterator.empty
-    else
-      val (current, next) = input.duplicate
-      current ++ repeatIter(count - 1, next)
+      0
