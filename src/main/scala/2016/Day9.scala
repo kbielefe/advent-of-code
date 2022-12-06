5f84b21019a35aa2bc4dd7a1090af6d606b49173
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2022-12-06 08:22:00 -0600

    Solve 2016/9 and 2022/6

--- src/main/scala/2016/Day9.scala
+++ src/main/scala/2016/Day9.scala
@@ -2,18 +2,21 @@ package advent2016
 
 object Day9:
   def part1(input: String): Long =
-    decompress(input.trim.iterator)
+    decompress(input.trim, 1)
 
   def part2(input: String): Long =
-    ???
+    decompress(input.trim, 2)
 
   private val markerRegex = """(\d+)x(\d+)""".r
 
-  private def decompress(input: Iterator[Char]): Long =
-    if input.hasNext then
-      val (raw, markerPlus) = input.span(_ != '(')
-      val (marker, remainder) = markerPlus.span(_ != ')')
-      val markerRegex(count, repeat) = marker.drop(1).mkString: @unchecked
-      raw.size + (repeat.toLong * count.toLong) + decompress(remainder.drop(1 + count.toInt))
-    else
+  private def decompress(input: String, version: Int): Long =
+    if input.isEmpty then
       0
+    else if input.head == '(' then
+      val marker = input.takeWhile(_ != ')').drop(1)
+      val remainder = input.dropWhile(_ != ')').drop(1)
+      val markerRegex(count, repeat) = marker: @unchecked
+      val repeated = if version == 1 then count.toLong else decompress(remainder.take(count.toInt), version)
+      repeat.toLong * repeated + decompress(remainder.drop(count.toInt), version)
+    else
+      1 + decompress(input.tail, version)
