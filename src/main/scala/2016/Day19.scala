6c21fd1244e28b88887aace30daedbc5d81d5911
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2022-12-10 14:44:37 -0600

    Solve 2016/19 part 2

--- src/main/scala/2016/Day19.scala
+++ src/main/scala/2016/Day19.scala
@@ -1,23 +1,32 @@
 package advent2016
 import scala.annotation.tailrec
+import scala.collection.immutable.Queue
 
 object Day19:
   def part1(input: Int): Int =
-    whiteElephant(Nil, List.fill(input + 1)(1).zipWithIndex.drop(1))
+    zipper(elfQueue(1, 1), elfQueue(2, input), 1, input - 1, 1)
 
   def part2(input: Int): Int =
-    ???
+    val victim = input / 2 + 1
+    zipper(elfQueue(1, victim - 1), elfQueue(victim, input), victim - 1, victim, 2)
+
+  private def elfQueue(from: Int, to: Int): Queue[Elf] =
+    Queue.range(from, to + 1).map(Elf(_, 1))
+
+  case class Elf(number: Int, presents: Int):
+    def steals(victim: Elf): Elf =
+      Elf(number, presents + victim.presents)
 
   @tailrec
-  private def whiteElephant(behind: List[(Int, Int)], ahead: List[(Int, Int)]): Int =
-    if behind.isEmpty && ahead.tail.isEmpty then
-      ahead.head._2
-    else if ahead.isEmpty then
-      whiteElephant(Nil, behind.reverse)
-    else if ahead.tail.isEmpty then
-      whiteElephant(Nil, ahead.head :: behind.reverse)
-    else if ahead.head._1 == 0 then
-      whiteElephant(behind, ahead.tail)
+  private def zipper(thieves: Queue[Elf], victims: Queue[Elf], thiefCount: Int, victimCount: Int, part: Int): Int =
+    val finished = thiefCount == 0 && victimCount == 1
+    val unbalanced =
+      (part == 2 && thiefCount < (thiefCount + victimCount) / 2) ||
+      (part == 1 && thiefCount == 0)
+
+    if finished then
+      victims.head.number
+    else if unbalanced then
+      zipper(thieves.appended(victims.head), victims.tail, thiefCount + 1, victimCount - 1, part)
     else
-      val (head :: next :: tail) = ahead: @unchecked
-      whiteElephant(((head._1 + next._1), head._2) :: behind, tail)
+      zipper(thieves.tail, victims.tail.appended(thieves.head.steals(victims.head)), thiefCount - 1, victimCount, part)
