27939f13b59fc07f939e204f866da27e08fab680
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2022-12-11 19:08:56 -0600

    Solve 2016/22 part 2

--- src/main/scala/2016/Day22.scala
+++ src/main/scala/2016/Day22.scala
@@ -3,9 +3,9 @@ import algorithms.AStar
 
 object Day22:
   def part1(input: List[String]): Int =
-    val nodes = input.drop(1).collect{
+    val nodes = input.drop(2).collect{
       case s"/dev/grid/node-x$x-y$y ${size}T ${used}T ${avail}T $p" =>
-        Node(size.trim.toInt, used.trim.toInt, avail.trim.toInt)
+        Node(x.trim.toInt, y.trim.toInt, size.trim.toInt, used.trim.toInt, avail.trim.toInt)
     }
     nodes.combinations(2).count{case List(a, b) => a.viable(b)} +
     nodes.combinations(2).count{case List(a, b) => b.viable(a)}
@@ -14,7 +14,7 @@ object Day22:
     val astar = AStar[Position, Int](goal, heuristic, neighborWeight, 0, _.neighbors)
     val nodes = input.drop(1).collect{
       case s"/dev/grid/node-x$x-y$y ${size}T ${used}T ${avail}T $p" =>
-        (x.toInt, y.toInt) -> Node(size.trim.toInt, used.trim.toInt, avail.trim.toInt)
+        (x.toInt, y.toInt) -> Node(x.trim.toInt, y.trim.toInt, size.trim.toInt, used.trim.toInt, avail.trim.toInt)
     }.toMap
     val dataX = nodes.map(_._1._1).max
     val dataY = 0
@@ -22,31 +22,39 @@ object Day22:
     val initialPosition = Position(dataX, dataY, blankX, blankY, nodes)
     astar.getMinCost(initialPosition).get
 
-  case class Node(size: Int, used: Int, avail: Int) derives CanEqual:
+  case class Node(x: Int, y: Int, size: Int, used: Int, avail: Int) derives CanEqual:
     def viable(other: Node): Boolean =
-      other != this && used != 0 && used <= other.avail
+      this != other && used != 0 && used <= other.avail
 
   case class Position(dataX: Int, dataY: Int, blankX: Int, blankY: Int, nodes: Map[(Int, Int), Node]) derives CanEqual:
+    override def equals(other: Any): Boolean =
+      val otherPos = other.asInstanceOf[Position]
+      otherPos.dataX == dataX && otherPos.dataY == dataY && otherPos.blankX == blankX && otherPos.blankY == blankY
+
+    override def hashCode: Int =
+      dataX.## + dataY.## + blankX.## + blankY.##
+
     override def toString: String =
       s"Position: ($dataX, $dataY)=${nodes(dataX -> dataY)} ($blankX, $blankY)=${nodes(blankX -> blankY)}"
 
+    val blankPos = (blankX, blankY)
+    val blankNode = nodes(blankPos)
+    val dataPos = (dataX, dataY)
+    val dataNode = nodes(dataPos)
+
     def neighbors: Set[Position] =
       Set((-1, 0), (1, 0), (0, -1), (0, 1)).flatMap{case (xOffset, yOffset) =>
-        val newBlank = (blankX + xOffset, blankY + yOffset)
-        nodes.get(newBlank) match
+        val newBlankPos = (blankX + xOffset, blankY + yOffset)
+        nodes.get(newBlankPos) match
           case Some(newBlankNode) =>
-            val blankNode = nodes((blankX, blankY))
             if blankNode.avail >= newBlankNode.used then
-              val newNodes =
-                nodes -
-                (blankX -> blankY) -
-                newBlank +
-                (newBlank -> (newBlankNode.copy(used = 0, avail = newBlankNode.size))) +
-                ((blankX, blankY) -> (blankNode.copy(used = newBlankNode.used, avail = blankNode.size - newBlankNode.used)))
-              val newBlankX = newBlank._1
-              val newBlankY = newBlank._2
-              val newDataX = if newBlankX == dataX && newBlankY == dataY then blankX else dataX
-              val newDataY = if newBlankX == dataX && newBlankY == dataY then blankY else dataY
+              val newNodes = nodes
+                .updated(newBlankPos, newBlankNode.copy(used = 0, avail = newBlankNode.size))
+                .updated(blankPos, blankNode.copy(used = newBlankNode.used, avail = blankNode.size - newBlankNode.used))
+              val newBlankX = newBlankPos._1
+              val newBlankY = newBlankPos._2
+              val newDataX = if newBlankPos == dataPos then blankX else dataX
+              val newDataY = if newBlankPos == dataPos then blankY else dataY
               Set(Position(newDataX, newDataY, newBlankX, newBlankY, newNodes))
             else
               Set.empty
