69f5622fae1ac042edb4ee8bdb3507c6835cb0fb
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2022-12-12 10:27:52 -0600

    Solve 2022/12

---
+++ src/main/scala/2022/Day23.scala
@@ -0,0 +1,64 @@
+package advent2016
+import scala.annotation.tailrec
+
+object Day23:
+  def part1(input: Vector[String]): Int =
+    execute(Map("a" -> 7, "b" -> 0, "c" -> 0, "d" -> 0, "pc" -> 0), input)("a")
+
+  def part2(input: Vector[String]): Int =
+    execute(Map("a" -> 12, "b" -> 0, "c" -> 1, "d" -> 0, "pc" -> 0), input)("a")
+
+  extension (s: String)
+    def isRegister: Boolean =
+      s == "a" || s == "b" || s == "c" || s == "d"
+
+  @tailrec
+  private def execute(registers: Map[String, Int], instructions: Vector[String]): Map[String, Int] =
+    def printReg(reg: String): String =
+      if reg.isRegister then
+        s"$reg(${registers(reg)})"
+      else
+        reg
+
+    if registers("pc") >= instructions.size then
+      registers
+    else
+      instructions(registers("pc")) match
+        case s"inc $x" =>
+          println(s"${registers("pc")}: inc ${printReg(x)}")
+          if x.isRegister then
+            execute(registers.updated(x, registers(x) + 1).updated("pc", registers("pc") + 1), instructions)
+          else
+            execute(registers.updated("pc", registers("pc") + 1), instructions)
+        case s"dec $x" =>
+          println(s"${registers("pc")}: dec ${printReg(x)}")
+          if x.isRegister then
+            execute(registers.updated(x, registers(x) - 1).updated("pc", registers("pc") + 1), instructions)
+          else
+            execute(registers.updated("pc", registers("pc") + 1), instructions)
+        case s"cpy $x $y" =>
+          println(s"${registers("pc")}: cpy ${printReg(x)} ${printReg(y)}")
+          if y.isRegister then
+            execute(registers.updated(y, registers.getOrElse(x, x.toInt)).updated("pc", registers("pc") + 1), instructions)
+          else
+            execute(registers.updated("pc", registers("pc") + 1), instructions)
+        case s"jnz $x $y" =>
+          println(s"${registers("pc")}: jnz ${printReg(x)} ${printReg(y)}")
+          if registers.getOrElse(x, x.toInt) == 0 then
+            execute(registers.updated("pc", registers("pc") + 1), instructions)
+          else
+            execute(registers.updated("pc", registers("pc") + registers.getOrElse(y, y.toInt)), instructions)
+        case s"tgl $x" =>
+          println(s"${registers("pc")}: tgl ${printReg(x)}")
+          val pcToToggle = registers("pc") + registers.getOrElse(x, x.toInt)
+          if pcToToggle < 0 || pcToToggle >= instructions.size then
+            execute(registers.updated("pc", registers("pc") + 1), instructions)
+          else
+            val toggled = instructions(pcToToggle) match
+              case s"inc $x"    => s"dec $x"
+              case s"dec $x"    => s"inc $x"
+              case s"tgl $x"    => s"inc $x"
+              case s"cpy $x $y" => s"jnz $x $y"
+              case s"jnz $x $y" => s"cpy $x $y"
+            val newInstructions = instructions.updated(pcToToggle, toggled)
+            execute(registers.updated("pc", registers("pc") + 1), newInstructions)
