13bc9145d40efedde48409f3509c39c8e2726d11
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2022-12-13 11:06:04 -0600

    Use Ordering instead.

--- src/main/scala/2022/Day13.scala
+++ src/main/scala/2022/Day13.scala
@@ -2,32 +2,23 @@ package advent2022
 import io.circe.Json
 import io.circe.parser.parse
 import puzzleparse.MultiLine
+import scala.math.Ordering
+import scala.math.Ordering.Implicits.*
 
 object Day13:
+  given Ordering[Json] with
+    def compare(a: Json, b: Json): Int =
+      val intCompare    = summon[Ordering[Int]].compare
+      val vectorCompare = summon[Ordering[Vector[Json]]].compare
+      (a.isNumber, b.isNumber) match
+        case ( true,  true) => intCompare(a.asNumber.flatMap(_.toInt).get, b.asNumber.flatMap(_.toInt).get)
+        case (false, false) => vectorCompare(a.asArray.get, b.asArray.get)
+        case (false,  true) => vectorCompare(a.asArray.get, Vector(b))
+        case ( true, false) => vectorCompare(Vector(a), b.asArray.get)
+
   def part1(input: MultiLine[List[Json]]): Int =
-    input.zipWithIndex.filter{case (List(left, right), index) => inOrder(left, right).get}.map(_._2 + 1).sum
+    input.zipWithIndex.filter{case (List(left, right), index) => left < right}.map(_._2 + 1).sum
 
   def part2(input: MultiLine[List[Json]]): Int =
     val dividerPackets = List("[[2]]", "[[6]]").map(parse).flatMap(_.toOption)
-    val sorted = (dividerPackets ++ input.flatten).sortWith((left, right) => inOrder(left, right).get)
-    sorted.zipWithIndex.filter(x => dividerPackets.contains(x._1)).map(_._2 + 1).product
-
-  private def inOrder(left: Json, right: Json): Option[Boolean] = (left.isNumber, right.isNumber) match
-    case (true, true)   => numbersInOrder(left, right)
-    case (false, false) => arraysInOrder(left, right)
-    case (false, true)  => arraysInOrder(left, Json.fromValues(List(right)))
-    case (true, false)  => arraysInOrder(Json.fromValues(List(left)), right)
-
-  private def numbersInOrder(left: Json, right: Json): Option[Boolean] =
-    val leftInt = left.asNumber.flatMap(_.toInt).get
-    val rightInt = right.asNumber.flatMap(_.toInt).get
-    if leftInt < rightInt then Some(true) else if rightInt < leftInt then Some(false) else None
-
-  private def arraysInOrder(left: Json, right: Json): Option[Boolean] =
-    val leftArray = left.asArray.get
-    val rightArray = right.asArray.get
-    (leftArray.headOption, rightArray.headOption) match
-      case (Some(left), Some(right)) => inOrder(left, right).orElse(inOrder(Json.fromValues(leftArray.tail), Json.fromValues(rightArray.tail)))
-      case (Some(left), None)        => Some(false)
-      case (None, Some(left))        => Some(true)
-      case (None, None)              => None
+    (dividerPackets ++ input.flatten).sorted.zipWithIndex.filter((packet, _) => dividerPackets.contains(packet)).map(_._2 + 1).product
