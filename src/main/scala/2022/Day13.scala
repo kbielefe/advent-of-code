ce199dd12b84a5cca2a84cf65bd4ec09228a0ece
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2022-12-13 20:32:50 -0600

    Solve 2015/6

--- src/main/scala/2022/Day13.scala
+++ src/main/scala/2022/Day13.scala
@@ -2,7 +2,6 @@ package advent2022
 import io.circe.Json
 import io.circe.parser.parse
 import puzzleparse.MultiLine
-import scala.math.Ordering
 import scala.math.Ordering.Implicits.*
 
 object Day13:
@@ -17,8 +16,9 @@ object Day13:
         case ( true, false) => vectorCompare(Vector(a), b.asArray.get)
 
   def part1(input: MultiLine[List[Json]]): Int =
-    input.zipWithIndex.filter{case (List(left, right), index) => left < right}.map(_._2 + 1).sum
+    input.zipWithIndex.filter{case (List(left, right), _) => left < right}.map(_._2 + 1).sum
 
   def part2(input: MultiLine[List[Json]]): Int =
     val dividerPackets = List("[[2]]", "[[6]]").map(parse).flatMap(_.toOption)
-    (dividerPackets ++ input.flatten).sorted.zipWithIndex.filter((packet, _) => dividerPackets.contains(packet)).map(_._2 + 1).product
+    val sorted = (dividerPackets ++ input.flatten).sorted
+    dividerPackets.map(sorted.indexOf).map(_ + 1).product
