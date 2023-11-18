377b7e1990d7eb1165241c4ff915fcbb241d0a32
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2023-11-18 13:21:28 -0600

    Implement fibonacci

--- algorithms/src/main/scala/Sequence.scala
+++ algorithms/src/main/scala/Sequence.scala
@@ -1,5 +1,9 @@
 package algorithms
 
+import math.Integral.Implicits.*
+
 object Sequence:
-  def fibonacci[N: Integral]: LazyList[N] = ???
+  def fibonacci[N](using n: Integral[N]): LazyList[N] =
+    n.zero #:: n.one #:: fibonacci[N].zip(fibonacci[N].tail).map((x, y) => x + y)
+
   def primes[N: Integral]: LazyList[N] = ???
