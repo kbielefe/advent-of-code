db8b08b52eb564fc7ee72cbf91d15585ee81a467
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2023-11-19 16:22:00 -0600

    Experiment with vehicle routing problem to solve 2022/16

--- 2022/src/main/scala/16.scala
+++ 2022/src/main/scala/16.scala
@@ -1,12 +1,62 @@
 package day16
+import algorithms.floydWarshall
 import parse.{*, given}
+import com.google.ortools.Loader
+import com.google.ortools.constraintsolver.*
+
+extension [A](i: Iterator[A])
+  def takeUntil(p: A => Boolean): Iterator[A] =
+    val (prefix, suffix) = i.span(x => !p(x))
+    prefix ++ suffix.take(1)
+
+extension(routing: RoutingModel)
+  def iterator(vehicle: Int, solution: Assignment): Iterator[Long] =
+    Iterator
+      .iterate(routing.start(vehicle))(i => solution.value(routing.nextVar(i)))
+      .takeUntil(routing.isEnd)
 
 case class Valve(name: String, rate: Int, tunnels: List[String] - ", ") derives ReadProduct
+
+case class Vertex(index: Int, opened: Boolean, valve: Valve)
+
 type I = List[Valve ~ """Valve ([A-Z][A-Z]) has flow rate=(\d+); tunnels? leads? to valves? (.+)"""] - "\n"
 
 object Puzzle extends runner.Day[I, Int, Int]:
   def part1(input: I): Int =
-    input.foreach(println)
+    val valveByName = input.map(valve => valve.name -> valve).toMap
+    val nodeByValveAndOpened = input.flatMap { valve =>
+      List((valve, false), (valve, true))
+    }.zipWithIndex.toMap
+    val valveAndOpenedByNode = nodeByValveAndOpened.map{case ((valve, opened), node) => (node, (valve, opened))}.toMap
+    val edges = input.flatMap {valve =>
+      val neighbors = valve.tunnels.map(valveByName.apply).map(valve => nodeByValveAndOpened(valve -> false))
+      val start = nodeByValveAndOpened(valve -> false)
+      val opened = nodeByValveAndOpened(valve -> true)
+      List((start, opened) -> 1, (opened, start) -> 0) ++ neighbors.flatMap {neighbor => List((start, neighbor) -> 1, (neighbor, start) -> 1)}
+    }.toMap
+    val floyd = floydWarshall(edges)
+    Loader.loadNativeLibraries()
+    val manager = new RoutingIndexManager(input.size * 2, 1, input.indexWhere(_.name == "AA") * 2)
+    val routing = new RoutingModel(manager)
+    val transitCallbackIndex = routing.registerTransitCallback{(fromIndex, toIndex) =>
+      val fromNode = manager.indexToNode(fromIndex)
+      val toNode = manager.indexToNode(toIndex)
+      val (valve, opened) = valveAndOpenedByNode(toNode)
+      if valve.name == "AA" && !opened then
+        0
+      else
+        floyd(fromNode -> toNode)
+    }
+    routing.setArcCostEvaluatorOfAllVehicles(transitCallbackIndex)
+    val searchParameters = main.defaultRoutingSearchParameters()
+      .toBuilder()
+      .setFirstSolutionStrategy(FirstSolutionStrategy.Value.PATH_CHEAPEST_ARC)
+      .build()
+
+    val solution = routing.solveWithParameters(searchParameters)
+
+    println(s"Objective: ${solution.objectiveValue()}")
+    routing.iterator(0, solution).map(manager.indexToNode).map(node => valveAndOpenedByNode(node) -> node).map{case ((valve, opened), node) => s"${valve.name}-${if opened then "o" else "c"}-$node"}.foreach(println)
     ???
 
   def part2(input: I): Int =
--- algorithms/src/main/scala/Tree.scala
+++ algorithms/src/main/scala/Tree.scala
@@ -7,58 +7,24 @@ import scala.reflect.{Typeable, TypeTest}
 trait Tree[A]:
   def children(node: A): Iterator[A]
 
-extension [A](a: A)(using t: Tree[A])
-  def children: Iterator[A] = t.children(a)
-
-  def depthFirstTraverse: Iterator[A] =
-    Iterator(a) ++ children.flatMap(_.depthFirstTraverse)
-
-  def breadthFirstTraverse: Iterator[A] =
-    def helper(queue: Iterator[A]): Iterator[A] =
-      if queue.hasNext then
-        val (q1, q2) = queue.duplicate
-        q1 ++ helper(q2.flatMap(t.children))
-      else
-        Iterator.empty
-    helper(Iterator(a))
-
-  def dfs(p: A => Boolean): Option[A] =
-    depthFirstTraverse.find(p)
-
-  def bfs(p: A => Boolean): Option[A] =
-    breadthFirstTraverse.find(p)
-
 object Tree:
-  inline given derived[A](using m: Mirror.Of[A]): Tree[A] =
-    inline m match
-      case s: Mirror.SumOf[A]     => deriveSum(s)
-      case p: Mirror.ProductOf[A] => deriveProduct(using p)
+  extension [A](a: A)(using t: Tree[A])
+    def children: Iterator[A] = t.children(a)
 
-  private inline def deriveSum[A](m: Mirror.SumOf[A]): Tree[A] =
-    new Tree[A]:
-      def children(node: A): Iterator[A] =
-        sumChildren[m.MirroredElemTypes, A](m, node, 0)
+    def depthFirstTraverse: Iterator[A] =
+      Iterator(a) ++ children.flatMap(_.depthFirstTraverse)
 
-  private inline def deriveProduct[A](using Mirror.ProductOf[A]): Tree[A] =
-    new Tree[A]:
-      def children(node: A): Iterator[A] =
-        productIterator[A, A](node)
-
-  private inline def sumChildren[Elem <: Tuple, A](m: Mirror.SumOf[A], node: A, ordinal: Int): Iterator[A] =
-    inline erasedValue[Elem] match
-      case _: EmptyTuple => Iterator.empty
-      case _: (Product *: t) =>
-        if m.ordinal(node) == ordinal then
-          inline erasedValue[Elem] match
-            case _: (h *: t) =>
-              productIterator[A, h](node.asInstanceOf[h]).asInstanceOf[Iterator[A]] ++ sumChildren[t, A](m, node, ordinal + 1)
+    def breadthFirstTraverse: Iterator[A] =
+      def helper(queue: Iterator[A]): Iterator[A] =
+        if queue.hasNext then
+          val (q1, q2) = queue.duplicate
+          q1 ++ helper(q2.flatMap(t.children))
         else
-          sumChildren[t, A](m, node, ordinal + 1)
-      case _: (h *: t) => sumChildren[t, A](m, node, ordinal + 1)
+          Iterator.empty
+      helper(Iterator(a))
 
-  private inline def productIterator[A, B](node: B): Iterator[A] =
-    node.asInstanceOf[Product].productIterator.map(_.asInstanceOf[Matchable]).flatMap{
-      case child: A                             => Iterator(child)
-      case children: IterableOnce[A] @unchecked => children.iterator
-      case _                                    => Iterator.empty
-    }
+    def dfs(p: A => Boolean): Option[A] =
+      depthFirstTraverse.find(p)
+
+    def bfs(p: A => Boolean): Option[A] =
+      breadthFirstTraverse.find(p)
