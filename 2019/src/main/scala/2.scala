60f11fedeaf6e3d3712a86ed44ef41dc27c020d5
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2023-11-20 13:33:28 -0600

    Solve 2019/2 part 2

--- 2019/src/main/scala/2.scala
+++ 2019/src/main/scala/2.scala
@@ -9,4 +9,12 @@ object Puzzle extends runner.Day[I, Int, Int]:
     IntCode(input.updated(1, 12).updated(2, 2)).runSync(0)
 
   def part2(input: I): Int =
-    ???
+    val nounsAndVerbs = for
+      noun <- (0 to 99).iterator
+      verb <- (0 to 99).iterator
+    yield (noun, verb)
+
+    nounsAndVerbs
+      .find((noun, verb) => IntCode(input.updated(1, noun).updated(2, verb)).runSync(0) == 19690720)
+      .map((noun, verb) => 100 * noun + verb)
+      .get
