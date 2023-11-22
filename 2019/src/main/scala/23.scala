545afd880b4b2a907ac07c07a766983b95829dc1
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2023-11-22 03:10:04 -0600

    Solve 2019/23 part 2

--- 2019/src/main/scala/23.scala
+++ 2019/src/main/scala/23.scala
@@ -15,15 +15,17 @@ object Puzzle extends runner.IODay[I, Long, Long]:
     nat    <- Part1Nat(nics)
     fibers <- forAllNics(i => nics(i).start(nat))
     result <- nat.result
-    _      <- fibers.traverse((x, y) => x.cancel >> y.cancel)
+    _      <- fibers.traverse((x, y) => x.cancel *> y.cancel)
   yield result
 
   def part2(input: I): IO[Long] = for
     nics   <- forAllNics(i => IntCode(input).flatMap(Nic(i)))
     nat    <- Part2Nat(nics)
     fibers <- forAllNics(i => nics(i).start(nat))
+    idle   <- nat.idleCheck.foreverM.start
     result <- nat.result
-    _      <- fibers.traverse((x, y) => x.cancel >> y.cancel)
+    _      <- idle.cancel
+    _      <- fibers.traverse((x, y) => x.cancel *> y.cancel)
   yield result
 
 case class Packet(destination: Long, x: Long, y: Long)
@@ -32,9 +34,9 @@ object Nic:
   def apply(networkAddress: Int)(computer: IntCode): IO[Nic] = for
     _   <- computer.input(networkAddress)
     ref <- Ref.of[IO, Int](0)
-  yield new Nic(computer, ref)
+  yield new Nic(networkAddress, computer, ref)
 
-class Nic private (inputBlockedComputer: IntCode, receiveCountSinceSend: Ref[IO, Int]):
+class Nic private (networkAddress: Int, inputBlockedComputer: IntCode, receiveCountSinceSend: Ref[IO, Int]):
   val computer = inputBlockedComputer.onInputBlock(triedToReceive)
 
   def start(nat: Nat): IO[(FiberIO[Unit], FiberIO[Nothing])] =
@@ -52,7 +54,7 @@ class Nic private (inputBlockedComputer: IntCode, receiveCountSinceSend: Ref[IO,
     (computer.inputIsEmpty, receivingWithoutSending).mapN(_ && _)
 
   def receivingWithoutSending: IO[Boolean] =
-    receiveCountSinceSend.get.map(_ >= 2)
+    receiveCountSinceSend.get.map(_ >= 100)
 
   def routePackets(nat: Nat): IO[Unit] = for
     destination <- computer.output
@@ -70,8 +72,11 @@ object Part1Nat:
     Deferred[IO, Long].map(new Part1Nat(nics, _))
 
 object Part2Nat:
-  def apply(nics: Vector[Nic]): IO[Nat] =
-    Deferred[IO, Long].map(new Part2Nat(nics, _))
+  def apply(nics: Vector[Nic]): IO[Part2Nat] = for
+    sent     <- Ref.of[IO, Option[Packet]](None)
+    received <- Ref.of[IO, Option[Packet]](None)
+    deferred <- Deferred[IO, Long]
+  yield new Part2Nat(nics, deferred, sent, received)
 
 class Part1Nat private (nics: Vector[Nic], deferred: Deferred[IO, Long]) extends Nat(deferred):
   override def send(packet: Packet): IO[Unit] =
@@ -80,12 +85,28 @@ class Part1Nat private (nics: Vector[Nic], deferred: Deferred[IO, Long]) extends
     else
       nics(packet.destination.toInt).send(packet)
 
-class Part2Nat(nics: Vector[Nic], deferred: Deferred[IO, Long]) extends Nat(deferred):
+class Part2Nat(nics: Vector[Nic], deferred: Deferred[IO, Long], lastPacketSent: Ref[IO, Option[Packet]], lastPacketReceived: Ref[IO, Option[Packet]]) extends Nat(deferred):
   override def send(packet: Packet): IO[Unit] =
     if packet.destination == 255 then
-      ???
+      lastPacketReceived.set(Some(packet))
     else
       nics(packet.destination.toInt).send(packet)
+
+  def idleCheck: IO[Unit] = for
+    idle     <- nics.traverse(_.idle).map(_.forall(identity))
+    _        <- IO.whenA(idle)(forward)
+  yield ()
+
+  def forward: IO[Unit] = for
+    received <- lastPacketReceived.get
+    sent     <- lastPacketSent.get
+    _        <- nics(0).send(received.get)
+    _        <- lastPacketSent.set(received)
+    _        <- IO.whenA(sentTwice(sent, received))(deferred.complete(sent.get.y).void)
+  yield ()
+
+  def sentTwice(sent: Option[Packet], received: Option[Packet]): Boolean =
+    (sent.map(_.y) == received.map(_.y)) && sent.isDefined
 
 def forAllNics[A](io: Int => IO[A]): IO[Vector[A]] =
   List.range(0, 50).parTraverse(io).map(_.toVector)
