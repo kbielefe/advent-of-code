d00a0620c8d2f433754f0623840125962b96207a
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2023-11-24 20:58:15 -0600

    Remove debug prints and caching.

--- 2019/src/main/scala/18.scala
+++ 2019/src/main/scala/18.scala
@@ -7,9 +7,8 @@ import scala.collection.mutable
 
 object Puzzle extends runner.Day[Grid, Int, Int]:
   def part1(input: Grid): Int =
-    println(input)
-    val allKeys = input.charSet.filter(_.isLower)
-    val pairs = (allKeys + '@').map(input.find(_).get).toList.combinations(2).flatMap{
+    val allKeys = input.charSet.filter(_.isLower) + '@'
+    val pairs = allKeys.map(input.find(_).get).toList.combinations(2).flatMap{
       case List(x, y) =>
         val path = input.aStar(y).getPath(x)
         val distance = path.size - 1 // path includes start
@@ -17,17 +16,13 @@ object Puzzle extends runner.Day[Grid, Int, Int]:
         List((input(x), input(y)) -> (distance, doorsAlongPath), (input(y), input(x)) -> (distance, doorsAlongPath))
       case _ => throw new Exception("Invalid combination of 2")
     }.toList.groupMap(_._1._1){case ((x, y) -> (distance, doors)) => (y, distance, doors)}
-    findAllKeys('@', pairs, allKeys + '@', Set.empty)
+    findAllKeys('@', pairs, allKeys, Set.empty)
 
   def part2(input: Grid): Int =
     ???
 
-  val cache: mutable.Map[(Char, Set[Char]), Int] = mutable.Map.empty
-
   def findAllKeys(current: Char, pairs: Map[Char, List[(Char, Int, Set[Char])]], allKeys: Set[Char], foundKeys: Set[Char]): Int =
-    if cache.contains((current, foundKeys)) then
-      cache((current, foundKeys))
-    else if (foundKeys + current) == allKeys then
+    if (foundKeys + current) == allKeys then
       0
     else
       val unlockedDoors = foundKeys.map(_.toUpper) + current.toUpper
@@ -35,12 +30,6 @@ object Puzzle extends runner.Day[Grid, Int, Int]:
         .filterNot(path => foundKeys.contains(path._1))
         .filter(path => (path._3 -- unlockedDoors).isEmpty)
         .map(x => (x._1, x._2))
-      if unblockedPaths.isEmpty then
-        println(current)
-        println(foundKeys.toList.sorted)
-        pairs(current).sortBy(_._1).map((dest, distance, set) => (dest, distance, set -- unlockedDoors)).foreach(println)
-      val result = unblockedPaths.map{case (next, distance) =>
+      unblockedPaths.map{case (next, distance) =>
         distance + findAllKeys(next, pairs, allKeys, foundKeys + current)
       }.min
-      cache += ((current, foundKeys) -> result)
-      result
