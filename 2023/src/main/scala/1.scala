f02581f90e66cc1bee465f7e089bd5b45db63f2e
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2023-11-30 23:55:35 -0600

    Solve 2023/1

--- 2023/src/main/scala/1.scala
+++ 2023/src/main/scala/1.scala
@@ -1,6 +1,39 @@
 package day1
-import parse.given
+import parse.{*, given}
 
-object Puzzle extends runner.Day[Int, Int, Int]:
-  def part1(input: Int): Int = input + 1
-  def part2(input: Int): Int = input + 2
+type I = List[String] - "\n"
+
+object Puzzle extends runner.Day[I, Int, Int]:
+  def part1(input: I): Int =
+    input.map(calibrationValue(false)).sum
+
+  def part2(input: I): Int =
+    input.map(calibrationValue(true)).sum
+
+  def calibrationValue(replaceWords: Boolean)(line: String): Int =
+    val d = digits(line, replaceWords)
+    d.head * 10 + d.last
+
+  val numberWords = List(
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+  )
+
+  val digitsByWord = numberWords.zipWithIndex.map((word, index) => (word, index + 1)).toMap
+
+  def digits(line: String, replaceWords: Boolean): List[Int] =
+    line.tails.filterNot(_.isEmpty).flatMap{part =>
+      if part.head.isDigit then
+        Some(part.head.asDigit)
+      else if replaceWords then
+        numberWords.flatMap(word => if part.startsWith(word) then digitsByWord.get(word) else None).headOption
+      else
+        None
+    }.toList
