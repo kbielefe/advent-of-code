87b3ff9a7ab871ed384cb0f83224608933e51451
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2023-12-01 23:26:10 -0600

    Solve 2023/2

--- 2023/src/main/scala/2.scala
+++ 2023/src/main/scala/2.scala
@@ -1,12 +1,34 @@
 package day2
 import parse.{*, given}
 
-type I = String
+case class Cubes(count: Int, color: String):
+  def possible: Boolean = color match
+    case "red"   => count <= 12
+    case "green" => count <= 13
+    case "blue"  => count <= 14
+
+case class Round(cubes: List[Cubes ~ """(\d+) (.+)"""] - ", "):
+  def possible: Boolean =
+    cubes.forall(_.possible)
+
+  def colorCount(color: String): Int =
+    cubes.find(_.color == color).map(_.count).getOrElse(0)
+
+case class Game(id: Int, rounds: List[Round ~ """(.+)"""] - "; "):
+  def possible: Boolean =
+    rounds.forall(_.possible)
+
+  def max(color: String): Int =
+    rounds.map(_.colorCount(color)).max
+
+  def power: Int =
+    List("red", "green", "blue").map(max).product
+
+type I = List[Game ~ """Game (\d+): (.+)"""] - "\n"
 
 object Puzzle extends runner.Day[I, Int, Int]:
   def part1(input: I): Int =
-    println(input)
-    ???
+    input.filter(_.possible).map(_.id).sum
 
   def part2(input: I): Int =
-    ???
+    input.map(_.power).sum
