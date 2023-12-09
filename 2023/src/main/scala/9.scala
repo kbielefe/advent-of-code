074203bd7eea6c3b010a7f31aebb4d73090fdabb
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2023-12-09 15:03:50 -0600

    Can just reverse.

--- 2023/src/main/scala/9.scala
+++ 2023/src/main/scala/9.scala
@@ -5,17 +5,14 @@ type I = List[List[Long] - " "] - "\n"
 
 object Puzzle extends runner.Day[I, Long, Long]:
   def part1(input: I): Long =
-    input.map(extrapolate(false)).sum
+    input.map(extrapolate).sum
 
   def part2(input: I): Long =
-    input.map(extrapolate(true)).sum
+    input.map(_.reverse).map(extrapolate).sum
 
-  def extrapolate(backwards: Boolean)(seq: List[Long]): Long =
+  def extrapolate(seq: List[Long]): Long =
     if seq.forall(_ == 0) then
       0
     else
       val diffs = seq.sliding(2).collect{case List(x, y) => y - x}
-      if backwards then
-        seq.head - extrapolate(true)(diffs.toList)
-      else
-        seq.last + extrapolate(false)(diffs.toList)
+      seq.last + extrapolate(diffs.toList)
