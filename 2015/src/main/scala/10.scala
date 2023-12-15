fc20c614cfab298132aaefc663c52cea2aaac0d8
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2023-12-14 22:25:06 -0600

    Solve 2015/10

--- 2015/src/main/scala/10.scala
+++ 2015/src/main/scala/10.scala
@@ -1,16 +1,28 @@
 package day10
 import parse.{*, given}
-import algorithms.*
+import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuffer
 
 object Puzzle extends runner.Day[String, Int, Int]:
   def part1(input: String): Int =
-    Iterator.iterate(input.map(_.asDigit).iterator)(lookAndSay).drop(40).size
+    val buffer = ArrayBuffer.from(input.map(_.asDigit.toByte))
+    lookAndSay(0, 0, 0, buffer, ArrayBuffer.empty, 40).size
 
   def part2(input: String): Int =
-    ???
+    val buffer = ArrayBuffer.from(input.map(_.asDigit.toByte))
+    lookAndSay(0, 0, 0, buffer, ArrayBuffer.empty, 50).size
 
-  def lookAndSay(in: Iterator[Int]): Iterator[Int] =
-    in.group.flatMap{group =>
-      val list = group.toList
-      Iterator(list.size, list.head)
-    }
+  @tailrec
+  def lookAndSay(index: Int, digit: Byte, count: Byte, in: ArrayBuffer[Byte], out: ArrayBuffer[Byte], row: Int): ArrayBuffer[Byte] =
+    if row == 0 then
+      in
+    else if index == 0 then
+      out.clear()
+      lookAndSay(1, in(0), 1, in, out, row)
+    else if index == in.size then
+      lookAndSay(0, 0, 0, out += count += digit, in, row - 1)
+    else if in(index) == digit then
+      lookAndSay(index + 1, digit, (count + 1).toByte, in, out, row)
+    else
+      lookAndSay(index + 1, in(index), 1, in, out += count += digit, row)
+
