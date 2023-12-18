5c1c8e104464baa69849ccff1f253f8cc90bd97d
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2023-12-18 12:00:26 -0600

    Solve 2023/18

---
+++ 2023/src/main/scala/18.scala
@@ -0,0 +1,35 @@
+package day18
+import parse.{*, given}
+import algorithms.{picks, shoelace}
+
+case class Dig(dir: Char, dist: Int, color: String):
+  def fromColor: Dig =
+    val newDist = Integer.parseInt(color.drop(1).take(5), 16)
+    val newDir = color.last match
+      case '0' => 'R'
+      case '1' => 'D'
+      case '2' => 'L'
+      case '3' => 'U'
+    Dig(newDir, newDist, color)
+
+given Read[Dig] = Read("""(U|D|L|R) (\d+) \((.+)\)""".r)
+
+type I = List[Dig] - "\n"
+
+object Puzzle extends runner.Day[I, Long, Long]:
+  def part1(input: I): Long =
+    answer(input)
+
+  def part2(input: I): Long =
+    answer(input.map(_.fromColor))
+
+  def answer(input: List[Dig]): Long =
+    val vertices = input.scanLeft((0L, 0L)){case ((row, col), dig) =>
+      dig.dir match
+        case 'U' => (row - dig.dist, col)
+        case 'D' => (row + dig.dist, col)
+        case 'L' => (row, col - dig.dist)
+        case 'R' => (row, col + dig.dist)
+    }.toVector.drop(1)
+    val border = input.map(_.dist).sum + 4
+    picks(shoelace(vertices), border)
---
+++ algorithms/src/main/scala/Shoelace.scala
@@ -0,0 +1,15 @@
+package algorithms
+import math.Integral.Implicits.infixIntegralOps
+
+/** Find the area of a polygon given its vertices in order. First vertex is also the last.
+ *  With integer values, need to use pick's theorem as this only finds internal points.
+ */
+def shoelace[N](points: Vector[(N, N)])(using n: Integral[N]): N =
+  n.abs((0 until points.size).map{i =>
+    val next = if i == (points.size - 1) then 0 else i + 1
+    (points(i)._1 * points(next)._2) - (points(next)._1 * points(i)._2)
+  }.sum / n.fromInt(2))
+
+/** Pick's theorem: Find total area of a polygon given number of interior points and points on the boundary */
+def picks[N](interior: N, boundary: N)(using n: Integral[N]): N =
+  interior + boundary / n.fromInt(2) - n.one
