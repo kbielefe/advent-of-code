472448708fd7dccec5b23c38e9c3d696a6192711
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2024-06-05 23:10:21 -0500

    2023/24 part 2 WIP

--- 2023/src/main/scala/24.scala
+++ 2023/src/main/scala/24.scala
@@ -40,7 +40,24 @@ object Puzzle extends runner.Day[List[Hailstone], BigInt, BigInt]:
   def part1(hailstones: List[Hailstone]): BigInt =
     hailstones
       .combinations(2)
-      .count{case List(first, second) => first.xyPathIntersects(second)}
+      .collect{case List(first, second) => first.xyPathIntersects(second)}
+      .count(identity)
 
   def part2(hailstones: List[Hailstone]): BigInt =
+    // px + t1*vx == h1.px + t1*h1.vx
+    // py + t1*vy == h1.py + t1*h1.vy
+    // pz + t1*vz == h1.pz + t1*h1.vz
+    // px + t2*vx == h2.px + t2*h2.vx
+    // py + t2*vy == h2.py + t2*h2.vy
+    // pz + t2*vz == h2.pz + t2*h2.vz
+    // px + t3*vx == h3.px + t3*h3.vx
+    // py + t3*vy == h3.py + t3*h3.vy
+    // pz + t3*vz == h3.pz + t3*h3.vz
+    // 9 equations, 9 unknowns!
+    // px py pz vx vy vz t1 t2 t3
+    //
+    // t1*vx - t1*h1.vx == h1.px - px
+    // t1 == (h1.px - px)/(vx-h1.vx) == (h1.py - py)/(vy - h1.vy)
+    // (h1.px - px)(vy - h1.vy) == (h1.py - py)(vx - h1.vx)
+    hailstones.take(3).foreach(println)
     ???
