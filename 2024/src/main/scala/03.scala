b1e5ab5b0b2338c5027917564c4e4e720c6bae59
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2024-12-03 12:03:01 -0600

    Factor out mul

--- 2024/src/main/scala/03.scala
+++ 2024/src/main/scala/03.scala
@@ -2,13 +2,14 @@ package day3
 import parse.{*, given}
 
 object Puzzle extends runner.Day[String, Int, Int]:
+  val mul = """mul\((\d{1,3}),(\d{1,3})\)""".r
+
   def part1(input: String): Int =
-    val products = """mul\((\d{1,3}),(\d{1,3})\)""".r.findAllMatchIn(input).map: m =>
+    val products = mul.findAllMatchIn(input).map: m =>
       m.group(1).toInt * m.group(2).toInt
     products.sum
 
   def part2(input: String): Int =
-    val mul = """mul\((\d+),(\d+)\)""".r
     val products = """mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)""".r.findAllIn(input).foldLeft((true, 0)):
       case ((true, sum), mul(left, right)) => (true, sum + left.toInt * right.toInt)
       case ((_, sum), "do()")              => (true, sum)
---
+++ 2024/src/main/scala/04.scala
@@ -0,0 +1,10 @@
+package day4
+import parse.{*, given}
+
+object Puzzle extends runner.Day[String, Int, Int]:
+  def part1(input: String): Int =
+    println(input)
+    ???
+
+  def part2(input: String): Int =
+    ???
