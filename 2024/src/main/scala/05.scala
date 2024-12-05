e91ea0b70b15cd89653b16be9955bfdf3b5d06cd
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2024-12-05 08:33:45 -0600

    Simplify due to input containing total ordering

--- 2024/src/main/scala/05.scala
+++ 2024/src/main/scala/05.scala
@@ -1,51 +1,29 @@
 package day5
 import parse.{*, given}
-import algorithms.{Edge, Graph}
-import scala.annotation.tailrec
 
-type Edges = Vector[(Int, Int)]
-case class Input(edges: Edges, updates: Vector[Vector[Int]])
+case class Input(rules: Set[String], updates: List[Vector[Int]]):
+  val ordering: Ordering[Int] = Ordering.fromLessThan((l, r) => rules.contains(s"$l|$r"))
 
-given Read[(Int, Int)] = Read("""(\d+)\|(\d+)""".r)
-given readEdges: Read[Edges] = Read("\n")
-given readPages: Read[Vector[Int]] = Read(",")
-given Read[Vector[Vector[Int]]] = Read("\n")
+given Read[Set[String]] = Read("\n")
+given Read[Vector[Int]] = Read(",")
+given Read[List[Vector[Int]]] = Read("\n")
 given Read[Input] = Read("\n\n")
 
 object Puzzle extends runner.Day[Input, Int, Int]:
   def part1(input: Input): Int =
+    given Ordering[Int] = input.ordering
     input.updates
-      .filter(inOrder(input.edges))
+      .filter(update => update == update.sorted)
       .map(middle)
       .sum
 
   def part2(input: Input): Int =
+    given Ordering[Int] = input.ordering
     input.updates
-      .filterNot(inOrder(input.edges))
-      .map(sort(input.edges))
+      .filterNot(update => update == update.sorted)
+      .map(_.sorted)
       .map(middle)
       .sum
 
-  def sort(edges: Edges)(update: Vector[Int]): Vector[Int] =
-    val graphEdges = relevantEdges(edges, update)
-      .map((from, to) => Edge(from, to, ()))
-    Graph.fromEdges(graphEdges).toposort.toVector
-
   def middle(update: Vector[Int]): Int =
     update(update.size / 2)
-
-  def relevantEdges(edges: Edges, update: Vector[Int]): Edges =
-    edges.filter((before, after) => update.contains(before) && update.contains(after))
-
-  def inOrder(edges: Edges)(update: Vector[Int]): Boolean =
-    @tailrec
-    def helper(edges: Edges, update: Vector[Int]): Boolean =
-      if update.isEmpty then
-        true
-      else if edges.map(_._2).contains(update.head) then
-        false
-      else
-        val newEdges = edges.filter((before, after) => before != update.head && after != update.head)
-        helper(newEdges, update.tail)
-
-    helper(relevantEdges(edges, update), update)
