5343f85bebc107b063d6fecefe7744b096b9578e
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2024-12-08 09:23:46 -0600

    Combine right and left

--- 2024/src/main/scala/08.scala
+++ 2024/src/main/scala/08.scala
@@ -11,17 +11,13 @@ object Puzzle extends runner.Day[Grid, Int, Int]:
     frequencies.flatMap(antinodes(grid, identity)).size
 
   def antinodes(grid: Grid, f: Iterator[Pos] => Iterator[Pos])(frequency: Char): Set[Pos] =
+    def iterate(left: Pos, right: Pos): Iterator[Pos] = Iterator.iterate(left): pos =>
+        Pos(pos.row - (right.row - left.row), pos.col - (right.col - left.col))
+      .takeWhile(grid.contains)
     grid.findAll(frequency)
       .toList
       .combinations(2)
       .flatMap:
-        case Seq(left, right) =>
-          val lefts = Iterator.iterate(left): pos =>
-              Pos(pos.row - (right.row - left.row), pos.col - (right.col - left.col))
-            .takeWhile(grid.contains)
-          val rights = Iterator.iterate(right): pos =>
-              Pos(pos.row - (left.row - right.row), pos.col - (left.col - right.col))
-            .takeWhile(grid.contains)
-          f(lefts) ++ f(rights)
+        case Seq(left, right) => f(iterate(left, right)) ++ f(iterate(right, left))
         case _ => ???
       .toSet
