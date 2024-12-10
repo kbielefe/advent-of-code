f8aec41a8fb0088a053873b0eceec0aeab42c460
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2024-12-09 19:05:56 -0600

    Slow but simpler solution to part 1

--- 2024/src/main/scala/09.scala
+++ 2024/src/main/scala/09.scala
@@ -1,18 +1,38 @@
 package day9
 import parse.{*, given}
 
-object Puzzle extends runner.Day[String, Long, Long]:
-  def part1(input: String): Long =
-    val blocks = input.map(_.asDigit).zipWithIndex.flatMap: (digit, index) =>
-      val isFile = index % 2 == 0
-      val id = index / 2
-      if isFile then List.fill(digit)(Some(id)) else List.fill(digit)(None)
-    val totalFileSize = blocks.count(_.isDefined)
-    val reversed = blocks.reverse.collect{case Some(id) => id}.toList
-    compact(blocks.iterator, reversed).take(totalFileSize).zipWithIndex.map(_.toLong * _).sum
-    
-  def part2(input: String): Long =
+type FileSystem = Vector[Option[Int]]
+
+given Read[FileSystem] = summon[Read[String]].map: input =>
+  input.toVector.map(_.asDigit).zipWithIndex.flatMap: (digit, index) =>
+    val isFile = index % 2 == 0
+    val id = index / 2
+    if isFile then List.fill(digit)(Some(id)) else List.fill(digit)(None)
+
+extension (blocks: FileSystem)
+  def availableFreeSpace(size: Int, leftOf: Int): Option[Long] =
+    blocks.take(leftOf).sliding(size).zipWithIndex.find(_._1.forall(!_.isDefined)).map(_._2)
+
+  def checksum: Long =
+    blocks.zipWithIndex.collect{case (Some(id), index) => id.toLong * index}.sum
+
+  def print: FileSystem =
+    println(blocks.map(_.map(_.toString).getOrElse(".")).mkString)
+    blocks
+
+  def compact: FileSystem =
+    blocks.zipWithIndex.reverse.foldLeft(blocks):
+      case (blocks, (Some(_), from)) => blocks.availableFreeSpace(1, from) match
+        case Some(to) => blocks.patch(to.toInt, blocks.slice(from, from + 1), 1).patch(from.toInt, List(None), 1)
+        case None => blocks
+      case (blocks, _) => blocks
+
+  def defrag: FileSystem =
     ???
 
-  def compact(blocks: Iterator[Option[Int]], reversed: List[Int]): Iterator[Int] =
-    blocks.takeWhile(_.isDefined).map(_.get) ++ Iterator(reversed.head) ++ compact(blocks, reversed.tail)
+object Puzzle extends runner.Day[FileSystem, Long, Long]:
+  def part1(blocks: FileSystem): Long =
+    blocks.compact.checksum
+    
+  def part2(blocks: FileSystem): Long =
+    blocks.defrag.checksum
