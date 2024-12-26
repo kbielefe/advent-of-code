4419d5c231c444c175b9256d9677b14096d2c7d4
Karl Bielefeldt <karl.bielefeldt@gmail.com>
2024-12-26 15:54:39 -0600

    Solve all 500!

--- 2024/src/main/scala/24 Crossed Wires.scala
+++ 2024/src/main/scala/24 Crossed Wires.scala
@@ -2,75 +2,70 @@ package day24
 import parse.{*, given}
 
 case class Gate(lhs: String, kind: String, rhs: String, output: String):
-  def signals: Set[String] =
-    Set(lhs, rhs, output)
-
   def calculate(input: Input): Boolean = kind match
     case "AND" => input.calculate(lhs) && input.calculate(rhs)
     case "OR" => input.calculate(lhs) || input.calculate(rhs)
     case "XOR" => input.calculate(lhs) ^ input.calculate(rhs)
     case _ => ???
 
-  def swap(left: String, right: String): Gate =
-    def helper(value: String): String =
-      if value == left then
-        right
-      else if value == right then
-        left
-      else
-        value
-    copy(output = helper(output))
+  override def toString: String =
+    s"$lhs $kind $rhs == $output"
+
+  def swap(l: String, r: String): Gate =
+    copy(output = if output == l then r else if output == r then l else output)
 
 case class Input(initial: Map[String, Int], gates: List[Gate]):
-  def signals: Set[String] =
-    gates.flatMap(_.signals).toSet
-
   def calculate(signal: String): Boolean =
     initial.get(signal).map(_ == 1)
       .orElse(gates.find(_.output == signal).map(_.calculate(this))).get
 
-  def allOutputs(signal: String): Set[String] =
-    Set(signal) ++ gates.find(_.output == signal).map(gate => allOutputs(gate.lhs) ++ allOutputs(gate.rhs)).getOrElse(Set.empty)
+  def swap(lhs: String, rhs: String): Input =
+    copy(gates = gates.map(_.swap(lhs, rhs)))
 
-  def zeroInputs: Input =
-    Input(initial.view.mapValues(_ => 0).toMap, gates)
+  def x(num: Int): Traversal =
+    new Signal("x%02d".format(num))
 
-  def withOne(signal: String): Input =
-    Input(initial + (signal -> 1), gates)
+  trait Traversal:
+    infix def and(char: Char): Traversal
+    infix def or(char: Char): Traversal
+    infix def xor(char: Char): Traversal
+    infix def xor(num: Int): List[String]
 
-  def containsLoop(visited: Set[String]): Boolean =
-    val inputs = signals.filter(_.startsWith("z")).flatMap:signal =>
-      val gate = gates.find(_.output == signal).get
-      List(gate.lhs, gate.rhs)
-    ???
+  object Empty extends Traversal:
+    infix def and(char: Char): Traversal = Empty
+    infix def or(char: Char): Traversal = Empty
+    infix def xor(char: Char): Traversal = Empty
+    infix def xor(num: Int): List[String] = List.empty
 
-  def allCorrect(swaps: List[(String, String)]): Boolean =
-    val input = swaps.foldLeft(this){case (input, (left, right)) => input.swap(left, right)}
-    !input.containsLoop(Set.empty) &&
-    input.signals.filter(_.startsWith("z")).forall(signal => input.swaps(signal).isEmpty)
+  class Signal(signal: String, accum: List[String] = List.empty) extends Traversal:
+    infix def and(char: Char): Traversal =
+      gates.find(gate => gate.kind == "AND" && (gate.lhs == signal || gate.rhs == signal)) match
+        case Some(gate) => new Signal(gate.output, gate.output :: accum)
+        case None =>
+          println(s"Couldn't find $signal AND _ == $char")
+          Empty
 
-  def swap(left: String, right: String): Input =
-    copy(gates = gates.map(_.swap(left, right)))
+    infix def or(char: Char): Traversal =
+      gates.find(gate => gate.kind == "OR" && (gate.lhs == signal || gate.rhs == signal)) match
+        case Some(gate) => new Signal(gate.output, gate.output :: accum)
+        case None =>
+          println(s"Couldn't find $signal OR _ == $char")
+          Empty
 
-  def swaps(signal: String): Set[(String, String)] =
-    zeroInputs.expectedSwap(false, signal) ++
-    zeroInputs.withOne("x" + signal.drop(1)).expectedSwap(true, signal) ++
-    zeroInputs.withOne("y" + signal.drop(1)).expectedSwap(true, signal) ++
-    zeroInputs.withOne("x" + signal.drop(1)).withOne("y" + signal.drop(1)).expectedSwap(false, signal)
+    infix def xor(char: Char): Traversal =
+      gates.find(gate => gate.kind == "XOR" && (gate.lhs == signal || gate.rhs == signal)) match
+        case Some(gate) => new Signal(gate.output, gate.output :: accum)
+        case None =>
+          println(s"Couldn't find $signal XOR _ == $char")
+          Empty
 
-  def expectedSwap(expected: Boolean, signal: String): Set[(String, String)] =
-    if calculate(signal) == expected then
-      Set.empty
-    else
-      val (ones, zeros) = allOutputs(signal)
-        .filterNot(output => "xyz".contains(output.head))
-        .partition(calculate)
-      val pairs = for
-        one  <- ones
-        zero <- zeros
-      yield one -> zero
-      pairs.filter((left, right) => swap(left, right).calculate(signal) == expected)
-        .map((l, r) => if l < r then (l, r) else (r, l))
+    infix def xor(num: Int): List[String] =
+      gates.find(gate => gate.kind == "XOR" && (gate.lhs == signal || gate.rhs == signal) && gate.output == "z%02d".format(num)) match
+        case Some(gate) => accum.reverse
+        case None =>
+          println(s"Couldn't find $signal XOR _ == ${"z%02d".format(num)}")
+          println(s"Found ${gates.filter(gate => gate.output == "z%02d".format(num) || gate.lhs == signal || gate.rhs == signal || gate.output == signal).mkString(", ")}")
+          List.empty
 
 given Read[Gate] = Read("""(\S+) (\S+) (\S+) -> (\S+)""".r)
 given Read[List[Gate]] = Read("\n")
@@ -80,10 +75,25 @@ given Read[Input] = Read("\n\n")
 
 object Puzzle extends runner.Day[Input, Long, String]:
   def part1(input: Input): Long =
-    input.signals.filter(_.startsWith("z")).toList.sorted.reverse.map(input.calculate).map(bool => if bool then 1L else 0L).reduceLeft(_ * 2 + _)
+    input.gates.map(_.output).filter(_.startsWith("z")).toList.sorted.reverse.map(input.calculate).map(bool => if bool then 1L else 0L).reduceLeft(_ * 2 + _)
 
   def part2(input: Input): String =
-    val result = input.signals.filter(_.startsWith("z")).flatMap(input.swaps)
-    val actualResult = result.toList.combinations(4).find(input.allCorrect).get.map((l, r) => List(l, r)).sorted
-    assert(actualResult.size == 8, "Must be 8 swaps")
-    actualResult.mkString(",")
+    val swapped = input
+      .swap("wjb", "cvp") // Correct
+      .swap("z34", "wcb") // Correct
+      .swap("z14", "qbw") // Correct
+      .swap("z10", "mkk") // Correct
+    (1 to 43).foreach: num =>
+      println(num)
+      println("a")
+      val a = swapped.x(num) xor 'A' xor num
+      println("acd")
+      val acd = swapped.x(num) xor 'A' and 'C' or 'D' xor (num + 1)
+      println("bd")
+      val bd = swapped.x(num) and 'B' or 'D' xor (num + 1)
+      println()
+      a.size == 1 && acd.size == 3 && bd.size == 2 &&
+      a(0) == acd(0) &&
+      acd(2) == bd(1)
+    // Found by manual inspection of the above output and guessing swaps with nearby signals
+    "cvp,mkk,qbw,wcb,wjb,z10,z14,z34"
